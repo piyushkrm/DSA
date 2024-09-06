b857ef0401bbf747c0c944f8cfdda34c65d2cf74
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-06 17:45:21 +0530

    Emplicit conversion

---
+++ Lecture2/typeCasting.cpp
@@ -0,0 +1,15 @@
+// write snippet
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Emplicit conversion
+    char grade = 'O';
+    int value = grade;
+    cout << "Value of grade " << grade << " is: " << value << endl;
+
+
+
+    return 0;
+}
