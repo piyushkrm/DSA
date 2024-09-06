389dd1156282fc3cbc3ae166102cfefa4c64b533
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-06 17:17:06 +0530

    Some changes are needed

--- Lecture2/code.cpp
+++ Lecture2/code.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main() {
 
-    cout << "Hello This is our second lecture....";
+    cout << "Hello Piyush\nThis is our second lecture....";
     return 0;
 }
+
+
+// g++ .\code.cpp
+// .\a.exe
+
+// upper and lower both are same
+// g++ code.cpp -o a.exe; ./a.exe
