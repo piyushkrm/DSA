37535ac29fb99bd14aa66b7dcfc6e1450755dc13
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-06 17:33:15 +0530

    Size of function to print size of integer

--- Lecture2/variables.cpp
+++ Lecture2/variables.cpp
@@ -4,8 +4,14 @@
 using namespace std;
 
 int main() {
+    // Integer
     int age = 18;
     cout << "Age : " << age << endl;
+    
+    // print the size of integers
+    cout << sizeof(age) << endl;
+
+
 
     return 0;
 }
