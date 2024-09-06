8cf5f8491378d48464f6e3fd12d2a92e6dc7828a
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-06 18:02:08 +0530

    Arithmetic operator are done

---
+++ Lecture2/operator.cpp
@@ -0,0 +1,22 @@
+// write simple code snippet
+
+#include <iostream>
+
+int main() {
+    int a = 5;
+    int b = 10;
+    // Arithmetic operations
+    int sum = a + b;
+    int difference = a - b;
+    int product = a * b;
+    int quotient = a / b;
+    int remainder = a % b;
+    // Output the results
+    std::cout << "Sum: " << sum << std::endl;
+    std::cout << "Difference: " << difference << std::endl;
+    std::cout << "Product: " << product << std::endl;
+    std::cout << "Quotient: " << quotient << std::endl;
+    std::cout << "Remainder: " << remainder << std::endl;
+
+    return 0;
+}
