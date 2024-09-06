8c7ca275f1a6a8228eec1c4ab7c384c4fe2539ff
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-06 17:59:09 +0530

    Input function done

---
+++ Lecture2/input.cpp
@@ -0,0 +1,16 @@
+// write simple code snippet
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    // Take the input
+    cout << "Enter any number : " << endl;
+    // cout << num <<endl; //print garbage collected
+    cin >> num;
+    // Print the inputted number
+    cout << "You entered: " << num << endl;
+    
+    return 0;
+}
