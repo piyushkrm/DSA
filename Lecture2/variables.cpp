ee04f925d256a2f64c90d28393ebd0981226b68e
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-06 17:34:41 +0530

    Float is done

--- Lecture2/variables.cpp
+++ Lecture2/variables.cpp
@@ -7,9 +7,16 @@ int main() {
     // Integer
     int age = 18;
     cout << "Age : " << age << endl;
-    
-    // print the size of integers
+
+    // print the size of integers(4)
     cout << sizeof(age) << endl;
+
+
+    // Float
+    float pi = 3.14;
+    cout << "Pi : " << pi << endl;
+    // print the size of floats(4)
+    cout << sizeof(pi) << endl;
 
 
 
