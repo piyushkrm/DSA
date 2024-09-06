abc5af473fb78001055c3f7882d75061aaea8a67
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-06 17:49:57 +0530

    Explicit conversion are done

--- Lecture2/typeCasting.cpp
+++ Lecture2/typeCasting.cpp
@@ -4,12 +4,19 @@
 using namespace std;
 
 int main() {
-    // Emplicit conversion
+    // Implicit conversion
     char grade = 'O';
     int value = grade;
     cout << "Value of grade " << grade << " is: " << value << endl;
 
+    // Explicit conversion
+    double weight = 65.5;
+    int weightInt = int(weight);
+    cout << "Weight in integer: " << weightInt << endl;
 
 
     return 0;
 }
+
+// g++ typeCasting.cpp -o a.exe; ./a.exe
+
