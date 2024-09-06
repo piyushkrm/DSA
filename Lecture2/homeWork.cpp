c8004e4f95889e9e90f1ffbb0b42af8c4a33f0ab
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-06 19:00:18 +0530

    Home work are done

---
+++ Lecture2/homeWork.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter 1st number : ";
+    cin >> a;
+    cout << "Enter 2nd number : ";
+    cin >> b;
+    
+
+    int sum = a + b;
+    int difference = a - b;
+    int product = a * b;
+    int quotient = a / b;
+    int remainder = a % b;
+    
+    // Output the results
+    std::cout << "Sum: " << sum << std::endl;
+    std::cout << "Difference: " << difference << std::endl;
+    std::cout << "Product: " << product << std::endl;
+    std::cout << "Quotient: " << quotient << std::endl;
+    std::cout << "Remainder: " << remainder << std::endl;
+
+    return 0;
+
+}
