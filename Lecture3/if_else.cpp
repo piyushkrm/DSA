c089e0a7b662c7109b5edb105a0ceb689d96f728
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-07 11:45:37 +0530

    If else done

--- Lecture3/if_else.cpp
+++ Lecture3/if_else.cpp
@@ -5,16 +5,11 @@ using namespace std;
 
 int main()
 {
-
     // If-else statements
     int x = 10;
-
-    if (x >= 0)
-    {
+    if (x >= 0) {
         cout << "The number is positive." << endl;
-    }
-    else
-    {
+    } else {
         cout << "The number is negative." << endl;
     }
 
@@ -22,12 +17,9 @@ int main()
     int a;
     cout << "Enter the number : ";
     cin >> a;
-    if (a % 2 == 0)
-    {
+    if (a % 2 == 0) {
         cout << "The number is even." << endl;
-    }
-    else
-    {
+    } else {
         cout << "The number is odd." << endl;
     }
 
