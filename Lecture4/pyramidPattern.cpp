7aed34289399c991a38eecb3f02c71ace7ee8317
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-07 18:31:25 +0530

    Pyramid pattern

---
+++ Lecture4/pyramidPattern.cpp
@@ -0,0 +1,29 @@
+
+// Pyramid pattern
+
+#include <iostream>
+using namespace std;    
+
+int main() {
+    int n = 6;
+
+    for (int i = 0; i < n; i++) {
+        // print space
+        for (int j = 0; j <= n-i-1; j++) {
+            cout << "  ";
+        }
+
+        // print numbers
+        for (int j = 1; j <= i+1; j++) {
+            cout<< j << " ";
+        }
+
+        // print number
+        for (int j = i; j > 0; j--) {
+            cout << j << " ";
+        }
+
+        cout << endl;
+    }
+
+}
