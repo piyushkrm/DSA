5edbd9077a6fddfb68da8fa4847c8de4141d5067
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-07 11:42:37 +0530

    Even odd

--- Lecture3/if_else.cpp
+++ Lecture3/if_else.cpp
@@ -7,15 +7,28 @@ int main()
 {
 
     // If-else statements
-    int a = 10;
+    int x = 10;
 
-    if (a >= 0)
+    if (x >= 0)
     {
         cout << "The number is positive." << endl;
     }
     else
     {
         cout << "The number is negative." << endl;
+    }
+
+    // Odd even
+    int a;
+    cout << "Enter the number : ";
+    cin >> a;
+    if (a % 2 == 0)
+    {
+        cout << "The number is even." << endl;
+    }
+    else
+    {
+        cout << "The number is odd." << endl;
     }
 
     return 0;
