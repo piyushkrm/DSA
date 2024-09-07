4ab407d3ad4eaf3955042a00819688a553eb0635
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-07 16:59:58 +0530

    Triangle pattern done

--- Lecture4/trianglePattern.cpp
+++ Lecture4/trianglePattern.cpp
@@ -6,15 +6,24 @@
 using namespace std;
 
 int main() {
-    int n = 4;
 
-    // for (int i = 0; i < n; i++) {
-    //     for (int j = 0; j <= i; j++) { 
-    //         cout << "* ";
-    //     }
-    //     cout << endl;
-    // }
+    // Question1
+    // int n = 4;
+    // *
+    // * *
+    // * * *
+    // * * * *
 
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) { 
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+
+    // Question2
     // 1 
     // 2 2 
     // 3 3 3 
@@ -23,6 +32,23 @@ int main() {
         for (int i = 0; i < n; i++) {
         for (int j = 0; j <= i; j++) { 
             cout << i+1 << " ";
+        }
+        cout << endl;
+    }
+
+
+
+    // Question3
+    // 1 
+    // 1 2
+    // 1 2 3
+    // 1 2 3 4
+    // 1 2 3 4 5
+
+    int x = 5;
+    for (int i = 1; i <= x; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << j <<" ";
         }
         cout << endl;
     }
