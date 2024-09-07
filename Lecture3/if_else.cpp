02ca0ddd69c86b349851cf43b38aa3ddfc7203c5
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-07 11:39:35 +0530

    This is if else statement whare i will chek a is negative or positive

---
+++ Lecture3/if_else.cpp
@@ -0,0 +1,22 @@
+// write a basics code snippet
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // If-else statements
+    int a = 10;
+
+    if (a >= 0)
+    {
+        cout << "The number is positive." << endl;
+    }
+    else
+    {
+        cout << "The number is negative." << endl;
+    }
+
+    return 0;
+}
