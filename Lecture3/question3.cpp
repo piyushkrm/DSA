7c84efe0f431d381793c62f7654bdc27832a049d
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-07 12:27:17 +0530

    sum of all odd numbers from 1 to n

---
+++ Lecture3/question3.cpp
@@ -0,0 +1,18 @@
+// sum of all odd numbers from 1 to n
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the n : ";
+    cin >> n;
+    int sum = 0;
+    for (int i = 1; i <= n; i += 2)
+    {
+        sum += i;
+    }
+    cout << "Sum of odd numbers from 1 to " << n << " is: " << sum << endl;
+    return 0;
+}
