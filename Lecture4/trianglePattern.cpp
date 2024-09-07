05f8a2f74fef7e90872f7b9454c4270a7ce7c4c1
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-07 16:42:21 +0530

    Triangle problem done

--- Lecture4/trianglePattern.cpp
+++ Lecture4/trianglePattern.cpp
@@ -8,9 +8,21 @@ using namespace std;
 int main() {
     int n = 4;
 
-    for (int i = 0; i < n; i++) {
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = 0; j <= i; j++) { 
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // 1 
+    // 2 2 
+    // 3 3 3 
+    // 4 4 4 4
+
+        for (int i = 0; i < n; i++) {
         for (int j = 0; j <= i; j++) { 
-            cout << "* ";
+            cout << i+1 << " ";
         }
         cout << endl;
     }
