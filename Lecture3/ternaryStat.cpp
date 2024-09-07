b9737edf68f8b6e4af2cdb93c20ae111ebe22099
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-07 12:00:55 +0530

    Ternary statement

---
+++ Lecture3/ternaryStat.cpp
@@ -0,0 +1,15 @@
+
+// Ternary Statement
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    cout << "Enter number  : ";
+    cin >> number;
+
+    cout << (number > 18 ? "Valid" : "Invalid");
+    
+    return 0;
+    
+}
