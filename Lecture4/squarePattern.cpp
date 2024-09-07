3a7cf72d46a6ac6b17e9730ea95d13e57a42d388
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-07 16:04:50 +0530

    Square with alphabet

--- Lecture4/squarePattern.cpp
+++ Lecture4/squarePattern.cpp
@@ -17,4 +17,22 @@ int main() {
         }
         cout << endl;
     }  
+
+        for (int i = 0; i < n; i++) {
+        char alpha = 'A';
+        for (int j = 0; j < n; j++) { 
+            cout << alpha <<" ";
+            alpha++;
+        }
+        cout << endl;
+        }
+
+        char alpha = 'A';
+        for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) { 
+            cout << alpha <<" ";
+            alpha++;
+        }
+        cout << endl;
+    }
 }
