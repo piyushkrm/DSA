9586cf2df5a32c8735354ddb049e55ebace3c656
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-07 12:09:02 +0530

    While loop

---
+++ Lecture3/loops.cpp
@@ -0,0 +1,16 @@
+// loop
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // while loop
+    int i = 0;
+    while (i <= 10) {
+        cout << i << " ";
+        i++;
+    }
+    cout << endl;
+
+    return 0;
+}
