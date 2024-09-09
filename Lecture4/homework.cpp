b5c722c0b8f4c4c512af626ac1670d9ba349a158
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-09 18:54:14 +0530

    Butterfly pattern done

--- Lecture4/homework.cpp
+++ Lecture4/homework.cpp
@@ -13,54 +13,61 @@ int main()
     // I J K L
     // M N O P
 
-    // int n = 3;
-    // char character = 'A';
+    int n = 3;
+    char character = 'A';
 
-    // for (int i = 0; i < n; i++) {
-    //     for (int j = 0; j < n; j++) {
-    //         cout << character << " ";
-    //         character++;
-    //     }
-    //     cout << endl;
-    // }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << character << " ";
+            character++;
+        }
+        cout << endl;
+    }
 
     // A
     // B B
     // C C C
     // D D D D
 
-    // int k = 4;
-    // char c = 'A';
+    int k = 4;
+    char c = 'A';
 
-    // for (int i = 0; i < k; i++) {
-    //     for (int j = 0; j <= i; j++) {
-    //         cout << c << " ";
-    //     }
-    //     cout << endl;
-    //     c++;
-    // }
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << c << " ";
+        }
+        cout << endl;
+        c++;
+    }
 
     // Butterfly pattern
 
-    int n = 5;
-    for (int i = 0; i < n; i++)
-    {
+    int n = 10;
+    for (int i = 0; i < n; i++) {
         // print 1st
-        for (int j = 0; j <= i; j++)
-        {
+        for (int j = 0; j <= i; j++) {
             cout << "* ";
         }
-
-        for (int j = 1; j < (2 * n - 2 * i - 1); j++)
-        {
+        for (int j = 1; j < (2 * n - 2 * i - 1); j++) {
             cout << "  ";
         }
-
-        for (int j = 0; j <= i; j++)
-        {
+        for (int j = 0; j <= i; j++) {
             cout << "* ";
         }
-
         cout << endl;
     }
+        // print 2nd
+    for (int i = n; i > 0; i--) {
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        for (int j = 0; j <= (2 * n - 2 * i - 1); j++) {
+            cout << "  ";
+        }
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
 }
