c2b1b18efe49bc2fa9a8ccbfbee168809aa710ca
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-09 11:38:56 +0530

    Top part done

--- Lecture4/pyramidPattern.cpp
+++ Lecture4/pyramidPattern.cpp
@@ -5,27 +5,53 @@
 using namespace std;    
 
 int main() {
-    int n;
-    cout << "Enter n : ";
-    cin >> n;
+    // int n;
+    // cout << "Enter n : ";
+    // cin >> n;
 
-    for (int i = 0; i < n; i++) {
+    // for (int i = 0; i < n; i++) {
+    //     // print space
+    //     for (int j = 0; j <= n-i-1; j++) {
+    //         cout << "  ";
+    //     }
+
+    //     // print numbers
+    //     for (int j = 1; j <= i+1; j++) {
+    //         cout<< j << " ";
+    //     }
+
+    //     // print number
+    //     for (int j = i; j > 0; j--) {
+    //         cout << j << " ";
+    //     }
+
+    //     cout << endl;
+    // }
+
+
+    // Hollow diamond pattern
+    // top part
+    int k = 5;
+    for (int i = 0; i < k; i++) {
         // print space
-        for (int j = 0; j <= n-i-1; j++) {
+        for (int j = 0; j < k - i - 1; j++) {
             cout << "  ";
         }
 
-        // print numbers
-        for (int j = 1; j <= i+1; j++) {
-            cout<< j << " ";
-        }
+        cout << "* ";
 
-        // print number
-        for (int j = i; j > 0; j--) {
-            cout << j << " ";
+        if (i != 0 ){
+            // print number
+            for (int j = 0; j < 2 * i - 1; j++) {
+                cout << "  ";
+            }
+            cout << "* ";
         }
-
         cout << endl;
+
+
+
+        
     }
 
 }
