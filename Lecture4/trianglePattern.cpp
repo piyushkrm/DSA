ba056fe3ad511cba14ea971b54c1d15b62c6319b
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-09 18:59:05 +0530

    Floyd's Triangle Pattern

--- Lecture4/trianglePattern.cpp
+++ Lecture4/trianglePattern.cpp
@@ -15,12 +15,12 @@ int main() {
     // * * * *
 
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= i; j++) { 
-            cout << "* ";
-        }
-        cout << endl;
-    }
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = 0; j <= i; j++) { 
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
 
 
     // Question2
@@ -29,12 +29,12 @@ int main() {
     // 3 3 3 
     // 4 4 4 4
 
-        for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= i; j++) { 
-            cout << i+1 << " ";
-        }
-        cout << endl;
-    }
+    //     for (int i = 0; i < n; i++) {
+    //     for (int j = 0; j <= i; j++) { 
+    //         cout << i+1 << " ";
+    //     }
+    //     cout << endl;
+    // }
 
 
 
@@ -45,13 +45,39 @@ int main() {
     // 1 2 3 4
     // 1 2 3 4 5
 
-    int x = 5;
-    for (int i = 1; i <= x; i++) {
-        for (int j = 1; j <= i; j++) {
-            cout << j <<" ";
+    // int x = 5;
+    // for (int i = 1; i <= x; i++) {
+    //     for (int j = 1; j <= i; j++) {
+    //         cout << j <<" ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // Question4
+        // A
+        // B A
+        // C B A
+        // D C B A
+        // E D C B A
+        
+
+        // Question5
+        // Floyd's Triangle Pattern
+        // A
+        // B C
+        // D E F
+        // G H I J
+        // K L M N O
+
+        int p = 5;
+        char ch = 'A';
+        for (int i = 1; i <= p; i++) {
+            for (int j = 1; j <= i; j++) {
+                cout << ch << " ";
+                ch++;
+            }
+            cout << endl;
         }
-        cout << endl;
-    }
 
     return 0;
 }
