176fe440cfb5b665aaee92b6285a0b933ca9455f
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-09 18:27:17 +0530

    butterfly 1st part done

--- Lecture4/homework.cpp
+++ Lecture4/homework.cpp
@@ -13,31 +13,44 @@ int main() {
     // M N O P
 
     
-    int n = 3;
-    char character = 'A';
+    // int n = 3;
+    // char character = 'A';
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << character << " ";
-            character++;
-        }
-        cout << endl;
-    }
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = 0; j < n; j++) {
+    //         cout << character << " ";
+    //         character++;
+    //     }
+    //     cout << endl;
+    // }
 
     // A
     // B B 
     // C C C 
     // D D D D 
 
-    int k = 4;
-    char c = 'A';
+    // int k = 4;
+    // char c = 'A';
 
-    for (int i = 0; i < k; i++) {
-        for (int j = 0; j <= i; j++) { 
-            cout << c << " ";
+    // for (int i = 0; i < k; i++) {
+    //     for (int j = 0; j <= i; j++) { 
+    //         cout << c << " ";
+    //     }
+    //     cout << endl;
+    //     c++;
+    // }
+
+    // Butterfly pattern
+
+    int n = 5;
+    for (int i = 1; i <= n; i++) {
+        // print 1st
+        for (int j = 1; j <=i; j++) {
+        cout << "* ";
         }
         cout << endl;
-        c++;
+        // for (int j = 0; j < (2 * n - 2 * i - 1); j++) {
     }
 
-}
+
+}   
