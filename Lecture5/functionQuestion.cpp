0f0b5411ae1426ef8d3db8a4da8ecac0b3d84d13
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-10 08:48:51 +0530

    Qs4. Calculate nCr binomial

--- Lecture5/functionQuestion.cpp
+++ Lecture5/functionQuestion.cpp
@@ -35,19 +35,47 @@ int sumOfDigits(int N) {
     return sum;
 }
 
+// Qs4. Calculate nCr binomial 
+// formula of nCr : (n! / (r! * (n-r)!))
+
+// factorial of n
+int fact(int x) {
+    if (x == 0 || x == 1) {
+        return 1;
+    }
+    int fact = 1;
+    for (int i = 2; i <= x; i++) {
+        fact *= i; 
+    }
+    return fact;
+}
+
+int nCr(int n, int r) {
+    return fact(n) / (fact(r)*(fact(n-r)));
+}
+
 int main() {
-    int num1;
-    cout << "Enter number to calculate sum from 1 : ";
-    cin >> num1;
-    cout << "Sum of numbers from 1 to " << num1 << " are : " << calSum(num1);
+    // int num1;
+    // cout << "Enter number to calculate sum from 1 : ";
+    // cin >> num1;
+    // cout << "Sum of numbers from 1 to " << num1 << " are : " << calSum(num1);
 
-    int num2;
-    cout << "\nEnter number to calculate factorial : ";
-    cin >> num2;
-    cout << "Factorial of " << num2 << " is : " << factorial(num2);
+    // int num2;
+    // cout << "\nEnter number to calculate factorial : ";
+    // cin >> num2;
+    // cout << "Factorial of " << num2 << " is : " << factorial(num2);
 
-    int num3;
-    cout << "\nEnter number to calculate sum of digits : ";
-    cin >> num3;
-    cout << "Sum of digits of " << num3 << " is : " << sumOfDigits(num3);
+    // int num3;
+    // cout << "\nEnter number to calculate sum of digits : ";
+    // cin >> num3;
+    // cout << "Sum of digits of " << num3 << " is : " << sumOfDigits(num3);
+    
+    int n, r;
+    cout << "\nEnter n for nCr binomial : ";
+    cin >> n;
+    cout << "Enter r for nCr binomial : ";
+    cin >> r;
+    cout << "nCr(" << n << "," << r << ") is : " << nCr(n, r);
+
+    
 }
