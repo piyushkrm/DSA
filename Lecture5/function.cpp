21bd87945b67e2b66339ccaff50e4130a849133e
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-10 06:47:46 +0530

    Function with parameter

--- Lecture5/function.cpp
+++ Lecture5/function.cpp
@@ -10,13 +10,15 @@ void printHello() {
     cout << "Hello, World!" << endl;
 }
 
+// function with parameter and i wiil calculate the sum of two numbers  
+int sum(int a, int b) {
+    return a + b;
+}
+
 int main() {
     // function calling
     printHello();
-    printHello();
-    printHello();
-    printHello();
-    printHello();
+    cout << "Sum of 2 and 3: " << sum(2, 3) << endl;
 
     return 0;
 }
