18c4231eec34cfa59500cc8349a5c255777fde69
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-10 07:13:43 +0530

    Question 2 is done

--- Lecture5/functionQuestion.cpp
+++ Lecture5/functionQuestion.cpp
@@ -13,10 +13,26 @@ int calSum(int N) {
     return sum;
 }
 
+// Qs2. Calculate factorial of a number N
+int factorial(int N) {
+    if (N == 0 || N == 1) {
+        return 1;
+    }
+    int fact = 1;
+    for (int i = 2; i <= N; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
 int main() {
     int num1;
     cout << "Enter number to calculate sum from 1 : ";
     cin >> num1;
     cout << "Sum of numbers from 1 to " << num1 << " are : " << calSum(num1);
 
+    int num2;
+    cout << "\nEnter number to calculate factorial : ";
+    cin >> num2;
+    cout << "Factorial of " << num2 << " is : " << factorial(num2);
 }
