999a7f95c69a4c316f479637a030e20ec2807bd4
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-10 17:58:42 +0530

    Homework problem done

---
+++ Lecture7/homeWorkProblem.cpp
@@ -0,0 +1,30 @@
+
+
+// 6 & 10
+// 6 | 10
+// 6 ^ 10
+// 10 << 2
+// 10 >> 1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 6 & 10
+    cout << (6& 10) << endl;
+
+    // 6 | 10
+    cout << (6 | 10) << endl;
+
+    // 6 ^ 10
+    cout << (6 ^ 10) << endl;
+
+    // 10 << 2
+    cout << (10 << 2) << endl;
+
+    // 10 >> 1
+    cout << (10 >> 1) << endl;
+
+    return 0;
+
+}
