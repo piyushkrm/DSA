8ed76ab4a19ba3cd30f21d6f2b7565743453d269
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-10 06:45:26 +0530

    Function lecture

---
+++ Lecture5/function.cpp
@@ -0,0 +1,22 @@
+
+
+// Function
+
+#include <iostream>
+using namespace std;
+
+// function definition
+void printHello() {
+    cout << "Hello, World!" << endl;
+}
+
+int main() {
+    // function calling
+    printHello();
+    printHello();
+    printHello();
+    printHello();
+    printHello();
+
+    return 0;
+}
