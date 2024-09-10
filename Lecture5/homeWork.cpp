d2b4c8a5b46013274271d4f6fcd8a4dc1afe1968
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-10 09:38:29 +0530

    Problem 2 is done

--- Lecture5/homeWork.cpp
+++ Lecture5/homeWork.cpp
@@ -2,8 +2,8 @@
 
 // Homework problems
 // Question1: WAF to check if number is prime or not
-// Question2:
-// Question3:
+// Question2: WAF to print all prime numbers from 2 to N;
+// Question3: WAF to print nth fibonacci series
 
 #include <iostream>
 using namespace std;
@@ -21,12 +21,28 @@ using namespace std;
         return true;
     }
 
+// Question2:
+    void printPrime(int num){
+        if(num <= 1){
+            cout << "ERROR";
+            return;
+        }
+        for(int i=2; i <= num; i++){ // Change condition to i <= num
+        if(isPrime(i)){
+            cout << i << " "; // Add space after printing each prime number
+            }
+        }
+    }
+
 int main(){
     // Question1
     int num;
-    cout << "Enter the number to check number is prime or not : ";
+    // cout << "Enter the number to check number is prime or not : ";
+    // cin >> num;
+    // cout << "Is " << num << " prime? " << (isPrime(num) ? "Yes" : "No") << endl;
+
+    // Question2
+    cout << "Enter the number to print prime numbers from 2 to N : ";
     cin >> num;
-    cout << "Is " << num << " prime? " << (isPrime(num) ? "Yes" : "No") << endl;
-
-
+    printPrime(num);
 }
