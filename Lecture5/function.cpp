756fc505267fa395f25d74e3cf263469435f4da0
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-10 06:58:16 +0530

    Function basics are done

--- Lecture5/function.cpp
+++ Lecture5/function.cpp
@@ -15,10 +15,16 @@ int sum(int a, int b) {
     return a + b;
 }
 
+// find minimum of two numbers
+int min(int a, int b) {
+    return (a > b)? a : b;
+}
+
 int main() {
     // function calling
     printHello();
     cout << "Sum of 2 and 3: " << sum(2, 3) << endl;
+    cout << "Minimum of 5 and 3: " << min(5, 3) << endl;
 
     return 0;
 }
