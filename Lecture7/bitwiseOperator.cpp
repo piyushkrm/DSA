278ef6ee59a65cae19c048b7c6b88b6e63047f99
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-10 17:25:19 +0530

    bitwise & operator

---
+++ Lecture7/bitwiseOperator.cpp
@@ -0,0 +1,17 @@
+//  bitwise operator
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a = 4;
+    int b = 8;
+    // bitwise & operator
+    int result = a & b;
+    cout << result;
+
+    // bitwise | operator
+    
+}
