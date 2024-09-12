db7a383c7d5e0170b018f3662340a4af7c1b4d4a
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-12 17:35:58 +0530

    Find min max

--- Lecture8/array.cpp
+++ Lecture8/array.cpp
@@ -38,8 +38,32 @@ int arrayInput()
     }
 }
 
+// Find max and min element in the array
+
+int maxMin() {
+    int arr[5] = {15, 20, 30, 40, 50};
+    int max = arr[0]; // Initialize max with the first element
+    int min = arr[0]; // Initialize min with the first element
+
+    for (int i = 1; i < 5; i++) { // Loop through the array
+        if (arr[i] > max) { // Check for max
+            max = arr[i];
+        }
+        if (arr[i] < min) { // Check for min
+            min = arr[i];
+        }
+    }
+
+    cout << "Minimum element in the array is : " << min << endl;
+    cout << "Maximum element in the array is : " << max << endl;
+
+    return 0;
+}
+
+
 int main()
 {
     // array();
-    arrayInput();
+    // arrayInput();
+    maxMin();
 }
