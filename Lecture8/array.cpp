ee9ef0ecd2414105d8e5087287740ae9721d0ebd
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-12 16:30:49 +0530

    Array initilization

---
+++ Lecture8/array.cpp
@@ -0,0 +1,13 @@
+
+
+// Array DSA Part 1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int Marks[5] = {15, 20, 30, 40, 50};
+    for (int i = 0; i < 5; i++) {
+        cout << "Marks[" << i << "] = " << Marks[i] << endl;
+    }
+}
