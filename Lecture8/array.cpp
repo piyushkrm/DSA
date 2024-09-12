25837cb105ede7ed871fd73efb772a54476e0ee3
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-12 16:47:51 +0530

    Array element are take from user

--- Lecture8/array.cpp
+++ Lecture8/array.cpp
@@ -5,9 +5,41 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+// array initilization
+int array()
+{
     int Marks[5] = {15, 20, 30, 40, 50};
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 5; i++)
+    {
         cout << "Marks[" << i << "] = " << Marks[i] << endl;
     }
+}
+
+// Array input from the users
+
+int arrayInput()
+{
+    int size;
+    cout << "Enter the size of an array : ";
+    cin >> size;
+    int arr[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Enter element at index " << i << " : ";
+        cin >> arr[i];
+    }
+
+    // print array elements
+    cout << "Array elements are : " << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Element at index " << i << " : " << arr[i] << endl;
+    }
+}
+
+int main()
+{
+    // array();
+    arrayInput();
 }
