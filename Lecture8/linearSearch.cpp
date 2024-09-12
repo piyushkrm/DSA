e7b8f9dbce0e90eeb10d0f15caddda6c82226cdd
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-12 19:59:27 +0530

    Reverse an array with swapping function

--- Lecture8/linearSearch.cpp
+++ Lecture8/linearSearch.cpp
@@ -27,6 +27,26 @@ int reverseArray(int array[], int size) {
         cout << reverse[i] << " ";
     }
 }
+
+// Reverse an array element using swap function
+int reverseArrayWithSwap(int array[], int size) {
+    int start = 0;
+    int end = size - 1;
+
+    for (int i = 0; i < size; i++) {
+        array[start] = array[end];
+        start++;
+        end--;
+    }
+
+    // print reverse array
+    for(int i = 0; i < size; i++) {
+        cout << array[i] << " ";
+    }
+
+}
+
+
 int main () {
     int array[] = {4, 2, 7,8, 1, 2, 5};
     int size = sizeof(array) / sizeof(array[0]);
@@ -34,4 +54,6 @@ int main () {
     // cout << "Target array element are available in the index of : " << linear_search(array, size, target) << endl;
 
     reverseArray(array, 7);
+    cout << "\n";
+    reverseArrayWithSwap(array, 7);
 }
