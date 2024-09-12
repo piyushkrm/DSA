57e0bfc7c4e1d9bb56a43c8085fc5a1c484cc61e
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-12 19:49:45 +0530

    Reverse an array

--- Lecture8/linearSearch.cpp
+++ Lecture8/linearSearch.cpp
@@ -14,9 +14,24 @@ int linear_search(int arr[], int size, int target) {
     return -1;
 }
 
+
+// Reverse an array element
+int reverseArray(int array[], int size) {
+    int reverse[size];
+
+    for (int i = 0; i < size; i++) {
+        reverse[size - i - 1] = array[i];
+    }
+
+    for(int i = 0; i < size; i++) {
+        cout << reverse[i] << " ";
+    }
+}
 int main () {
     int array[] = {4, 2, 7,8, 1, 2, 5};
     int size = sizeof(array) / sizeof(array[0]);
-    int target = 0;
-    cout << "Target array element are available in the index of : " << linear_search(array, size, target) << endl;
+    // int target = 0;
+    // cout << "Target array element are available in the index of : " << linear_search(array, size, target) << endl;
+
+    reverseArray(array, 7);
 }
