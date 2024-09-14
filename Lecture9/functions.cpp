b7031870ac6be6eeebca47e955a2323477618515
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-14 10:19:40 +0530

    size of a vector

---
+++ Lecture9/functions.cpp
@@ -0,0 +1,15 @@
+
+// vector functions
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    vector <int> vector = {10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    // size of a vector
+    cout << "Size of vector: " << vector.size() << endl;
+
+    
+}
