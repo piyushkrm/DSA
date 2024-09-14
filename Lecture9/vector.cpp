6600b9c1fbc72b57d24d102a328b01c7e1e288d3
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-14 09:43:50 +0530

    Vector declaration approach1

---
+++ Lecture9/vector.cpp
@@ -0,0 +1,13 @@
+
+
+// vector
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // Approach 1: Declare and initialize a vector
+    vector<int> vec;
+    cout << vec[0];
+}
