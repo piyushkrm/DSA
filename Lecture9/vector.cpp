ba15b9fef717bf240b8786c2d8cb552e4bee37a7
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-14 09:47:03 +0530

    Approach 2: Declare and initialize a vector with values

--- Lecture9/vector.cpp
+++ Lecture9/vector.cpp
@@ -9,5 +9,9 @@ using namespace std;
 int main() {
     // Approach 1: Declare and initialize a vector
     vector<int> vec;
-    cout << vec[0];
+    // cout << vec[0];
+
+    // Approach 2: Declare and initialize a vector with values
+    vector<int> vec2 = {1, 2, 3, 4, 5};
+    cout << vec2[2];
 }
