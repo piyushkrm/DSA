73e370eb40cc5ea704e0ee1370c785aba32d42fd
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-14 09:56:53 +0530

    Approach 3: Declare and initialize a vector with size of vector an values using

--- Lecture9/vector.cpp
+++ Lecture9/vector.cpp
@@ -9,9 +9,17 @@ using namespace std;
 int main() {
     // Approach 1: Declare and initialize a vector
     vector<int> vec;
-    // cout << vec[0];
+    // cout << vec[0] << endl;
 
     // Approach 2: Declare and initialize a vector with values
     vector<int> vec2 = {1, 2, 3, 4, 5};
-    cout << vec2[2];
+    cout << vec2[2] << endl;
+
+    // Approach 3: Declare and initialize a vector with size of vector an values using
+    vector<int> vec3(10, 0);
+    for (int i = 0; i < 10; i++) {
+        cout << vec3[i] << " ";
+    }
+
+
 }
