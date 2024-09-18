7e09573f75414a570c6309d72dc70e56265cd639
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-18 10:30:50 +0530

    Calculate max of sub array using brute force approach

--- Lecture10/subArray.cpp
+++ Lecture10/subArray.cpp
@@ -3,12 +3,14 @@
 // print subarray of an array
 
 #include <iostream>
+#include <vector>
+#include <climits>
 
 using namespace std;
 
-void printSubarray(int arr[], int n) {
-    for (int start = 0; start < n; ++start) {
-        for (int end = start; end < n; ++end) {
+void printSubArray(int arr[], int n) {
+    for (int start = 0; start < n; start++) {
+        for (int end = start; end < n; end++) {
             for (int i = start; i <= end; i++) {
                 cout << arr[i] << " ";
             }
@@ -19,8 +21,25 @@ void printSubarray(int arr[], int n) {
 
 }
 
+void maxSumOfSubArray(int arr[], int n) {
+    int maxSum = INT_MIN;
+    for (int start = 0; start < n; start++) {
+    int currentSum = 0;
+        for (int end = start; end < n; end++) {
+                currentSum += arr[end];
+                maxSum = max(maxSum, currentSum);
+        }
+    }
+    cout << "Maximum sum of a subArray is: " << maxSum << endl;
+
+}
+
 int main() {
     int n = 8;
     int array[8] = {0, 1, 2, 3, 4, 5, 6, 7};
-    printSubarray(array, n);
+    // printSubArray(array, n);
+
+    int arr[]= {3, -4, 5, 4, -1, 7, -8};
+    maxSumOfSubArray(arr, 7);
+    return 0;
 }
