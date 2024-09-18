179d398fa1636ca42462eac7c084d3e8e825261b
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-18 11:19:19 +0530

    Kadane's Algorithm

--- Lecture10/subArray.cpp
+++ Lecture10/subArray.cpp
@@ -7,6 +7,14 @@
 #include <climits>
 
 using namespace std;
+/**
+ * The function `printSubArray` prints all possible subarrays of an input array.
+ * 
+ * @param arr The `arr` parameter is an integer array that contains the elements of the array for which
+ * you want to print all possible subarrays.
+ * @param n The parameter `n` in the `printSubArray` function represents the size of the array `arr[]`,
+ * indicating the number of elements in the array that you want to consider for printing subarrays.
+ */
 
 void printSubArray(int arr[], int n) {
     for (int start = 0; start < n; start++) {
@@ -18,8 +26,19 @@ void printSubArray(int arr[], int n) {
         }
         cout << endl;
     }
-
 }
+
+
+/**
+ * The function calculates the maximum sum of a subarray within a given array.
+ * 
+ * @param arr The function `maxSumOfSubArray` takes an array `arr` of integers and its size `n` as
+ * input parameters. It calculates the maximum sum of a subarray within the given array using a nested
+ * loop approach.
+ * @param n The parameter `n` in the `maxSumOfSubArray` function represents the size of the array
+ * `arr[]`, which is the number of elements in the array. This parameter is used to determine the
+ * length of the array and iterate over its elements to find the maximum sum of a subarray
+ */
 
 void maxSumOfSubArray(int arr[], int n) {
     int maxSum = INT_MIN;
@@ -34,12 +53,30 @@ void maxSumOfSubArray(int arr[], int n) {
 
 }
 
+// Find maximum of an sub array using Kadane's Algorithm
+int maxSubArray(int arr[], int n) {
+    int currentSum = 0;
+    int maxSum = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        currentSum += arr[i];
+        maxSum = max(maxSum, currentSum);
+
+        if (currentSum < 0) {
+            currentSum = 0;
+        }
+    }
+    cout << "Maximum sum of a subArray is: " << maxSum << endl;
+}
+
+
 int main() {
     int n = 8;
     int array[8] = {0, 1, 2, 3, 4, 5, 6, 7};
     // printSubArray(array, n);
 
-    int arr[]= {3, -4, 5, 4, -1, 7, -8};
-    maxSumOfSubArray(arr, 7);
+    int arr[8]= {3, -4, 5, 4, -1, 7, -8};
+    // maxSumOfSubArray(arr, 7);
+    maxSubArray(arr, 8);
     return 0;
 }
