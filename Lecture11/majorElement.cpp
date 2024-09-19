75b4c46ac3cce1d20109afdbb11ff620c69736a7
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-19 14:54:12 +0530

    Optimized approach

--- Lecture11/majorElement.cpp
+++ Lecture11/majorElement.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -26,8 +27,37 @@ int findMajorElements(vector<int> &array) {
     return -1;
 }
 
+
+// Optimized approach
+int findMajorEle(vector<int> &array) {
+
+    int n = array.size();
+
+    // sorting
+    sort(array.begin(), array.end());
+
+    // frequency
+    int freq = 1;
+    int ans = array[0];
+
+    // iteration
+    for (int i = 1; i < n; i++) {
+        if (array[i] == array[i - 1]) {
+            freq++;
+        } else {
+            freq = 1;
+            ans = array[i];
+        }
+        if (freq > n / 2) {
+            return ans;
+        }
+    }
+    return -1;
+}
+
 int main() {
     vector<int> array = {1, 2, 2, 1, 1, 2, 2};
-    cout << findMajorElements(array) << endl;
+    // cout << findMajorElements(array) << endl;
+    cout << findMajorEle(array) << endl;
     return 0;
 }
