90fcc717a508a95fac70e88b7f8f61c26dab21a4
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-19 11:16:35 +0530

    Done with optiized way

--- Lecture11/pairSum.cpp
+++ Lecture11/pairSum.cpp
@@ -22,11 +22,34 @@ vector<int> pairSum(vector<int> array, int target) {
     return ans;  // return empty vector if no pair found
 }
 
+// optimized approach
+vector<int> sumPair(vector<int> array, int target) {
+    int i = 0;
+    int j = array.size() - 1;
+    vector<int> ans;
+    
+    while (i < j) {
+        int presentSum = array[i] + array[j];
+
+        if (presentSum > target) {
+            j--;
+        } else if (presentSum < target) {
+            i++;
+        } else {
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;  // return pair found
+        }
+    }
+}
+
 int main() {
     vector<int> array = {2, 7, 11, 15};
     int target = 13;
     
-    vector<int> ans = pairSum(array, target);
+    vector<int> ans = pairSum(array, 26);
+    vector<int> ANS = sumPair(array, 9);
     cout << ans[0] << ", "<< ans[1] << endl;
+    cout << ANS[0] << ", "<< ANS[1] << endl;
     return 0;
 }
