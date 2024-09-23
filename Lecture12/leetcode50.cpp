dac79fa2554f3044b047a91ec4c881fe30eb6404
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-23 17:29:38 +0530

    Solved

--- Lecture12/leetcode50.cpp
+++ Lecture12/leetcode50.cpp
@@ -1,3 +1,30 @@
 
 // 50. Pow(x, n)
 // Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+double myPow(double x, int n) {
+
+    long binaryForm = n;
+    double ans = 1;
+    while (binaryForm > 0) {
+        if (binaryForm % 2 == 1) {
+            ans *= x;
+        }
+        x *= x;
+        binaryForm /= 2;
+    }
+    return ans;
+}
+
+int main() {
+    double x = 2.00000;
+    int n = 10;
+    cout << "Result: " << myPow(x, n) << endl;
+    return 0;
+ 
+}
