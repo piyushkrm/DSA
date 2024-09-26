e27f05b35ed2c47d8392a4aedd1b2d047d9007d2
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-26 12:55:31 +0530

    Done with optimized way

--- Lecture14/leetcode238.cpp
+++ Lecture14/leetcode238.cpp
@@ -21,13 +21,55 @@ vector <int> productExceptSelf(vector<int>& nums) {
     return result;
 }
 
+// Optimal solution for the same problem
+vector<int> productExceptSelfOptimized(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> result(n, 1);
+    vector<int> prefix(n, 1);
+    vector<int> suffix(n, 1);
+
+    // prefix
+    for (int i = 1; i < n; i++) {
+        prefix[i] = prefix[i - 1] * nums[i - 1];
+    }
+
+    // suffix
+    for (int i = n - 2; i >= 0; i--) {
+        suffix[i] = suffix[i + 1] * nums[i + 1];
+    }
+
+    // result
+    for (int i = 0; i < n; i++) {
+        result[i] = prefix[i] * suffix[i];
+    }
+
+    return result;
+}
+
 int main() {
     vector<int> nums = {1, 2, 3, 4};
-    // Function returns
+
+    // Approach 1
+    // Function calling
     vector<int> result = productExceptSelf(nums);
+    cout << "Product of array except self is: ";
     // Print result
     for (int num : result) {
         cout << num << " ";
     }
+    cout << endl;
+
+    // Approach 2
+    // Calling function 
+    vector<int> ans = productExceptSelfOptimized(nums);
+    cout << "Product of array except self is: ";
+    // Print result
+    for (int num : ans) {
+        cout << num << " ";
+    }
+
+    // Approach 3
+    // Calling functions
+    
 }
 
