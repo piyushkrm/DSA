70a6acdb888467fbb9f0fcc3138e5dad414fb85e
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-26 13:10:30 +0530

    reduce space complexity for same problem

--- Lecture14/leetcode238.cpp
+++ Lecture14/leetcode238.cpp
@@ -46,6 +46,25 @@ vector<int> productExceptSelfOptimized(vector<int>& nums) {
     return result;
 }
 
+// reduce space complexity
+vector<int> productExceptSelfOptimizedSC(vector<int> &nums) {
+    int n = nums.size();
+    vector<int> result(n, 1);
+
+    for (int i = 1; i < n; i++) {
+        result[i] = result[i - 1] * nums[i - 1];
+    }
+
+    int suffix = 1;
+    for (int i = n - 2; i >= 0; i--) {
+        suffix *= nums[i + 1]; 
+        result[i] *= suffix;
+
+    }
+
+    return result;
+}
+
 int main() {
     vector<int> nums = {1, 2, 3, 4};
 
@@ -67,9 +86,17 @@ int main() {
     for (int num : ans) {
         cout << num << " ";
     }
+    cout << endl;
+
 
     // Approach 3
     // Calling functions
-    
+    vector<int> resultSC = productExceptSelfOptimizedSC(nums);
+    cout << "Product of array except self is: ";
+    // Print result
+    for (int num : resultSC) {
+        cout << num << " ";
+    }
+
 }
 
