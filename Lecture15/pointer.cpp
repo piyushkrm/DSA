e2169f8efe15c70aea3431d9ea46a6026809f349
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-30 09:17:44 +0530

    Array pointer

--- Lecture15/pointer.cpp
+++ Lecture15/pointer.cpp
@@ -14,15 +14,21 @@ int changeValueAlias(int &value) {
     value = 500;
 }
 
+// Array Pointer
+int pointerArr() {
+    int arr[] = {10, 1, 2, 3, 4, 5};
+    cout << "Array pointer " << *arr << endl;
+}
+
 
 int main() {
     int a = 10;
     int* ptr = &a;
 
-    // cout << "Value of a: " << a << endl;
-    // cout << "Address of a: " << &a << endl;
-    // cout << "Value of ptr: " << ptr << endl;
-    // cout << "Dereferenced value of ptr: " << *ptr << endl;
+    cout << "Value of a: " << a << endl;
+    cout << "Address of a: " << &a << endl;
+    cout << "Value of ptr: " << ptr << endl;
+    cout << "Dereferenced value of ptr: " << *ptr << endl;
 
     // call by reference
     changeValue(a);
@@ -31,4 +37,7 @@ int main() {
     // call by reference using alias
     changeValueAlias(a);
     cout << "After changeValueAlias: a = " << a << endl;
+
+    // Array Pointer
+    pointerArr();
 }
