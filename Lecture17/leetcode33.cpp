5c3aca46689a614dbbb204902bc727cf608c19a1
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-04 10:05:59 +0530

    Leetcode problem 33 is solved

--- Lecture17/leetcode33.cpp
+++ Lecture17/leetcode33.cpp
@@ -1,4 +1,4 @@
-// leetcode 33. Search in Rotated Sorted Array
+// leet code 33. Search in Rotated Sorted Array
 
 #include <iostream>
 #include <vector>
@@ -10,7 +10,21 @@ int search(vector<int>& nums, int target) {
     while (left <= right) {
         int mid = left + (right - left) / 2;
         if (nums[mid] == target) return mid;
-        
+        if (nums[left] <= nums[mid]) {  //left side is  sorted
+            if (nums[left] <= target && target <= nums[mid]) {
+                right = mid - 1;    // target is in the left half
+            } else {
+                left = mid + 1;     // target is in the right half
+            } 
+        } else { //right side is sorted
+                if (nums[mid] <= target && target <= nums[right]) {
+                    left = mid + 1;  // target is in the right half
+                } else {
+                    right = mid - 1; // target is in the left
+                }
+            }
+        } 
+    return -1;
 }
 
 int main() {
