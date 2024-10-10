393d0a4a9a0264f00e18b6178240d6c49830a411
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-10 11:11:58 +0530

    Book allocation problem is solved

--- Lecture19/bookAllocation.cpp
+++ Lecture19/bookAllocation.cpp
@@ -1,56 +1,76 @@
-
-// Book allocation problem
-
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-
 bool isValid(vector<int> &book, int n, int m, int maxAllocatedPage) {
     int student = 1;
     int allocatedPage = 0;
 
-    for (int i = 0; i <n; i++) {
+    for (int i = 0; i < n; i++) {
         if (book[i] > maxAllocatedPage) {
             return false;
         }
         if (allocatedPage + book[i] > maxAllocatedPage) {
             student++;
             allocatedPage = book[i];
+            if (student > m) {
+                return false;
+            }
         } else {
             allocatedPage += book[i];
         }
     }
-    return student > n ? false : true;
+    return true;
 }
 
-int allocationBooks(vector<int> & books, int n, int m) {
+int allocationBooks(vector<int> &books, int n, int m) {
     if (n > m) {
         return -1;
     }
 
-    //sum of an array of books
     int sum = 0;
+    int maxBook = 0;
     for (int i = 0; i < n; i++) {
         sum += books[i];
+        maxBook = max(maxBook, books[i]);
     }
 
-    int st = 0;
+    int st = maxBook;
     int end = sum;
     int ans = -1;
 
     while (st <= end) {
         int mid = (st + end) / 2;
-        if (isValid(books, n, m)) {
+        cout << "Checking mid: " << mid << ", st: " << st << ", end: " << end << endl;
+
+        if (isValid(books, n, m, mid)) {
             ans = mid;
+            cout << "Valid mid found: " << mid << endl;
             end = mid - 1;
         } else {
+            cout << "Invalid mid: " << mid << endl;
             st = mid + 1;
         }
-        return ans;
     }
+    
+    cout << "Final answer: " << ans << endl; // Additional debug info
+    return ans;
 }
-int main() {
 
+
+int main() {
+    vector<int> books = {12, 34, 67, 90};
+    int n = 4;
+    int m = 2;
+    int testMaxAllocatedPage = 113; // Try with different values like 100, 150
+
+    if (isValid(books, n, m, testMaxAllocatedPage)) {
+        cout << "Valid allocation for max pages: " << testMaxAllocatedPage << endl;
+    } else {
+        cout << "Invalid allocation for max pages: " << testMaxAllocatedPage << endl;
+    }
+
+    return 0;
 }
+
