4b9abe049821a031c42e195ed4561066b4d52bc3
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-14 11:10:52 +0530

    Sort an array using sort function

---
+++ Lecture23/aggressiveCow.cpp
@@ -0,0 +1,22 @@
+
+
+// Aggressive cows problem
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int getDistance(vector<int>& points, int N, int M) {
+    short(points.begin(), points.end());
+
+    
+}
+
+bool isPossible(vector<int>& points, int N, int minAllowedDistance) {
+
+}
+
+int main() {
+
+}
