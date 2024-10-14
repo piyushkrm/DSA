dbb4fd675cf6ad3a0538099f11dac20f091f38fd
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-14 11:13:46 +0530

    getDistance function is complete

--- Lecture23/aggressiveCow.cpp
+++ Lecture23/aggressiveCow.cpp
@@ -7,10 +7,26 @@
 
 using namespace std;
 
-int getDistance(vector<int>& points, int N, int M) {
+int getDistance(vector<int>& points, int N, int C) {
     short(points.begin(), points.end());
 
-    
+    int start = 1;
+    int end = points[N - 1] - points[0];
+    int ans = -1;
+
+    while(start <= end) {
+        int mid = start + (end - start) / 2;
+
+        if (isPossible(points, N, C, mid)) {
+            // right searching
+            ans = mid;
+            start = mid + 1;
+        } else {
+            // left searching
+            end = mid - 1;
+        }
+    }
+    return -1;
 }
 
 bool isPossible(vector<int>& points, int N, int minAllowedDistance) {
