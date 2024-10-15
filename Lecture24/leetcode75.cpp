9797e3df7870d15da162639bfb31b8944c14cc39
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-15 12:25:47 +0530

    main function almost done

---
+++ Lecture24/leetcode75.cpp
@@ -0,0 +1,25 @@
+
+
+// Sort an array of 0's 1's and 2's 
+
+    // Approach:
+    // 1. Initialize three pointers: low, mid, high. low points to the first element, mid points to the current element being checked, and high points to the last element.
+    // 2. Iterate through the array:
+    //  - If the current element is 0, swap it with the element at low, then increment both low and mid.
+    //  - If the current element is 1, increment mid.
+    //  - If the current element is 2, swap it with the element at high, then decrement high.
+    // 3. After the iteration, the array will be sorted in ascending order.
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> arr = {2, 0, 1, 2, 0, 1, 0, 2, 1, 0};
+    return 0;
+}
