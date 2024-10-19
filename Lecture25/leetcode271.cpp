b045d8ad5582b6e2582ec54e1748ba314b8372f5
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-19 12:57:22 +0530

    Leetcode problem 25

---
+++ Lecture25/leetcode271.cpp
@@ -0,0 +1,16 @@
+
+
+// 217. Contains Duplicate
+
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+int main() {
+    vector<int> nums = {1, 2, 3, 1};
+    bool result = containsDuplicate(nums);
+    cout << result << endl;
+    return 0;
+}
