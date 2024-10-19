95cd442a935170053f49850363006ac164fa3db3
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-19 12:58:45 +0530

    function complete

--- Lecture25/leetcode271.cpp
+++ Lecture25/leetcode271.cpp
@@ -8,6 +8,19 @@
 
 using namespace std;
 
+bool containsDuplicate(vector<int>& nums) {
+    int n = nums.size();
+
+    for (int i = 0; i < n - 1;  i++) {
+        for (int j = 0; j < n; j++) {
+            if (nums[i] == nums[j]) {
+                return true;
+            }
+        }
+    }
+    return false;  // No duplicates found  // Time complexity: O(n^2)
+}
+
 int main() {
     vector<int> nums = {1, 2, 3, 1};
     bool result = containsDuplicate(nums);
