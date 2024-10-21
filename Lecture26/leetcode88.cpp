2086d288d3229b619a06dc4f7c6c3acad16ffabb
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-21 15:25:09 +0530

    merge sorted function complete

--- Lecture26/leetcode88.cpp
+++ Lecture26/leetcode88.cpp
@@ -1,3 +1,31 @@
 
 // 88. Merge Sorted Array
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int mergeSorted(vector<int>& a, vector<int>& b) {
+    int n = a.size();
+    int m = b.size();
+    int i = n - 1;
+    int j = m - 1;
+    int index = n + m - 1;
+
+    while (i >= 0 && j >= 0) {
+        if (a[i] > b[j]) {
+            a[index] = a[i];
+            i--;
+        } else {
+            a[index] = b[j];
+            j--;
+        }
+        index--;
+    }
+}
+
+int main() {
+
+}
