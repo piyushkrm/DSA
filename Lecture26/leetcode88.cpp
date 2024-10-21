0bd85e369d849194038c3fbf6bab784c6a71eed2
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-21 15:30:13 +0530

    Complete

--- Lecture26/leetcode88.cpp
+++ Lecture26/leetcode88.cpp
@@ -1,4 +1,3 @@
-
 // 88. Merge Sorted Array
 
 #include <iostream>
@@ -7,33 +6,38 @@
 
 using namespace std;
 
-int mergeSorted(vector<int>& a, vector<int>& b) {
-    int n = a.size();
-    int m = b.size();
-    int i = n - 1;
-    int j = m - 1;
-    int index = n + m - 1;
+void mergeSorted(vector<int>& a, int n, vector<int>& b, int m) {
+    int i = n - 1;  // Last element in the initial part of a
+    int j = m - 1;  // Last element in b
+    int index = n + m - 1;  // Last position in a
 
     while (i >= 0 && j >= 0) {
-        if (a[i] > b[j]) {
-            a[index] = a[i];
-            i--;
+        if (a[i] >= b[j]) {
+            a[index--] = a[i--];
         } else {
-            a[index] = b[j];
-            j--;
+            a[index--] = b[j--];
         }
-        index--;
+    }
+    while (j >= 0) {
+        a[index--] = b[j--];
     }
 }
 
 int main() {
-    // call the function
-    vector<int> a = {1, 3, 5, 7, 0, 0, 0};
+    // The size of non-zero elements in 'a' and the total size of 'b'
+    vector<int> a = {1, 3, 5, 7, 0, 0, 0, 0};
     vector<int> b = {2, 4, 6, 8};
-    mergeSorted(a, b);
+    
+    int n = 4; // Number of non-zero elements in 'a'
+    int m = b.size(); // Size of 'b'
+
+    mergeSorted(a, n, b, m);
+
+    // Print the merged array
     cout << "Merged array: ";
     for (int i = 0; i < a.size(); i++) {
         cout << a[i] << " ";
     }
+
     return 0;
 }
