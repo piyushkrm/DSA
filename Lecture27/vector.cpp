0f2d26d41538fd803b908bf1da05309bd58b3feb
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-22 10:02:29 +0530

    The code ector<int> newVec(5, 10); creates a vector named ewVec with 5 elements, each initialized to the value 10.

--- Lecture27/vector.cpp
+++ Lecture27/vector.cpp
@@ -1,6 +1,3 @@
-
-// C++ STL (Standard Template Library)
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -8,7 +5,6 @@
 using namespace std;
 
 // print vector elements
-
 void printVector(vector<int> vec) {
     cout << "Vector elements are : ";
     for (int i = 0; i < vec.size(); i++) {
@@ -25,7 +21,7 @@ int main() {
     cout << "Size of vector : "<< vec.size() << endl;
     cout << endl;
 
-    // push back
+    // push_back
     vec.push_back(10);
     vec.push_back(20);
     vec.push_back(30);
@@ -37,10 +33,10 @@ int main() {
     // pop_back
     vec.pop_back();
     printVector(vec);
-    cout << "Size of vector after push_back : " << vec.size() << endl;
+    cout << "Size of vector after pop_back : " << vec.size() << endl;
     cout << endl;
 
-    //capacity
+    // capacity
     cout << "Capacity of vector : " << vec.capacity() << endl;
     cout << endl;
 
@@ -61,8 +57,12 @@ int main() {
     cout << endl;
 
     // Initialize the vector
-    vector<int> vector = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    printVector(vector);
+    vector<int> newVector = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    printVector(newVector);
+
+    // The code `vector<int> newVec(5, 10);` creates a vector named `newVec` with 5 elements, each initialized to the value 10.
+    vector<int> newVec(5, 10);
+    printVector(newVec);
 
     cout << endl;
     return 0;
