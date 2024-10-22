e1b2aa87f469be2adaf2640645a143a7862893ac
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-22 10:12:05 +0530

    Add more elements

--- Lecture27/vector.cpp
+++ Lecture27/vector.cpp
@@ -1,3 +1,10 @@
+/**
+ * The code demonstrates various operations on vectors in C++, such as adding, removing elements,
+ * accessing elements by index, and initializing vectors.
+ * 
+ * @param vec The code snippet you provided demonstrates various operations on vectors in C++. Here's a
+ * breakdown of the vector operations performed in the code:
+ */
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -25,6 +32,26 @@ int main() {
     vec.push_back(10);
     vec.push_back(20);
     vec.push_back(30);
+    // add more elements
+    vec.push_back(40);
+    vec.push_back(50);
+    vec.push_back(60);
+    vec.push_back(70);
+    vec.push_back(80);
+    vec.push_back(90);
+    vec.push_back(100);
+    // add more elements (capacity exceeded)
+    vec.push_back(110);
+    vec.push_back(120);
+    vec.push_back(130);
+    vec.push_back(140);
+    vec.push_back(150);
+    vec.push_back(160);
+    vec.push_back(170);
+    vec.push_back(180);
+    vec.push_back(190);
+    vec.push_back(200);
+
     printVector(vec);
 
     cout << "Size of vector after push_back : " << vec.size() << endl;
@@ -68,6 +95,7 @@ int main() {
     vector<int> vec01 = {1, 2, 3, 4, 5, 6, 7, 8};
     vector<int> vec02(vec01);
     printVector(vec02);
+
 
     cout << endl;
     return 0;
