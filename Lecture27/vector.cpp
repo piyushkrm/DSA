d617662ab964de4660865786b5a9e4287105c97f
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-22 09:18:34 +0530

    Size of a vector

---
+++ Lecture27/vector.cpp
@@ -0,0 +1,15 @@
+
+// C++ STL (Standard Template Library)
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    vector<int> vec;
+
+    cout << "Size of vector : "<< vec.size() << endl;
+    return 0;
+}
