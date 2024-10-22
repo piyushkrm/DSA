acb2fc9130278f57c92c51ef9171696591a8fae7
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-22 10:37:04 +0530

    Vector iterator(begin)

---
+++ Lecture27/vectorIterators.cpp
@@ -0,0 +1,18 @@
+
+// vector iterators
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> vec = {1, 2, 3, 4, 5};
+
+    // begin
+    cout << *(vec.begin()) << endl;  // 1
+
+
+
+
+    return 0;
+}
