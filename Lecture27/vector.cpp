f0e4ca5a92f533e1abedcbc2e4d824287349b216
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-22 09:21:41 +0530

    Use printVector function to print all elements of the vector

--- Lecture27/vector.cpp
+++ Lecture27/vector.cpp
@@ -7,9 +7,28 @@
 
 using namespace std;
 
+// print vector elements
+
+void printVector(vector<int> vec) {
+    cout << "Vector elements are : ";
+    for (int i = 0; i < vec.size(); i++) {
+        cout << vec[i] << " ";
+    }
+    cout << endl;
+}
+
 int main() {
     vector<int> vec;
+    printVector(vec);
 
+    // Size
     cout << "Size of vector : "<< vec.size() << endl;
+
+    // push back
+    vec.push_back(10);
+    vec.push_back(20);
+    vec.push_back(30);
+    printVector(vec);
+    cout << "Size of vector after push_back : " << vec.size() << endl;
     return 0;
 }
