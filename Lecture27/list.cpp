4aa2124d5ae23981af61b7860362ebdfafc3a2e3
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-23 09:47:45 +0530

    Print list function done

---
+++ Lecture27/list.cpp
@@ -0,0 +1,26 @@
+
+
+// list container
+
+#include <iostream>
+#include <list>
+using namespace std;
+
+// Function to print elements of a list
+
+void printList(list<int>& list) {
+    for (int ls : list) {
+        cout << ls << " ";
+    }
+    cout << endl;
+}
+
+
+void listFunctions(list<int>& list) {
+
+}
+
+int main() {
+    list<int> myList = {1, 2, 3, 4, 5};
+
+}
