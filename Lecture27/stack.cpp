c54125693c82cda4ad863cb1704cd775adf6d41e
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-24 17:35:08 +0530

    I'm add all the elements on the stack using the push function

--- Lecture27/stack.cpp
+++ Lecture27/stack.cpp
@@ -8,5 +8,15 @@ using namespace std;
 int main() {
     stack<int> stack = {10, 50, 15, 26, 59, 78, 95};
 
-    
+
+    // push the element in the stack
+    stack.push(10);
+    stack.push(50);
+    stack.push(15);
+    stack.push(26);
+    stack.push(59);
+    stack.push(78);
+    stack.push(95);
+
+
 }
