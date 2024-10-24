a4cdfe04ac1e95d83af02c60b05aedeff7dc21cb
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-24 17:46:31 +0530

    swapping two stack

--- Lecture27/stack.cpp
+++ Lecture27/stack.cpp
@@ -16,44 +16,56 @@ void printStack(stack<int> stack) {
 }
 
 int main() {
-    stack<int> stack;
+    stack<int> stack1;
 
 
     // push the element in the stack
-    stack.push(10);
-    stack.push(50);
-    stack.push(15);
-    stack.push(26);
-    stack.push(59);
-    stack.push(78);
-    stack.push(95);
+    stack1.push(10);
+    stack1.push(50);
+    stack1.push(15);
+    stack1.push(26);
+    stack1.push(59);
+    stack1.push(78);
+    stack1.push(95);
 
     // print the stack elements
     cout << "Original Stack: ";
-    printStack(stack);
+    printStack(stack1);
 
     //emplace_stack
-    stack.emplace(30);
-    stack.emplace(60);
-    stack.emplace(80);
-    stack.emplace(100);
+    stack1.emplace(30);
+    stack1.emplace(60);
+    stack1.emplace(80);
+    stack1.emplace(100);
 
     // print the stack elements after emplace_stack
     cout << "Stack after emplace_stack: ";
-    printStack(stack);
+    printStack(stack1);
 
     // top of stack
-    cout << "Top element of stack: " << stack.top() << endl;
+    cout << "Top element of stack: " << stack1.top() << endl;
 
     //pop the stack element
-    stack.pop();
+    stack1.pop();
     cout << "Stack after pop: ";
-    printStack(stack);
+    printStack(stack1);
 
     // size of stack
-    cout << "Size of stack: " << stack.size() << endl;
+    cout << "Size of stack: " << stack1.size() << endl;
 
     // empty
-    cout << "Is stack empty? : " << (stack.empty()? "Yes" : "No") << endl;
+    cout << "Is stack empty? : " << (stack1.empty()? "Yes" : "No") << endl;
+
+    //swap stack
+    stack<int> stack2;
+    stack2.push(40);
+    stack2.push(70);
+    stack2.push(100);
+    stack2.push(130);
+    stack1.swap(stack2);
+    cout << "Stack1 after swap: ";
+    printStack(stack1);
+    cout << "Stack2 after swap: ";
+    printStack(stack2);
 
 }
