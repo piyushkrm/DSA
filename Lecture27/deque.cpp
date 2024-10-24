efb459c07f677b9b708d43477663a40573b7eb55
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-24 09:52:23 +0530

    push_back function

--- Lecture27/deque.cpp
+++ Lecture27/deque.cpp
@@ -7,5 +7,11 @@
 using namespace std;
 
 int main() {
-    
+    deque<int> queue;
+
+    // push_back
+    queue.push_back(10);
+    queue.push_back(20);
+    queue.push_back(30);
+    queue.push_back(40);
 }
