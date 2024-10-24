5c6df1d119b1cce9ce86cfc17cbd5778ae9a7b71
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-24 11:26:55 +0530

    vector pair

--- Lecture27/pair.cpp
+++ Lecture27/pair.cpp
@@ -3,6 +3,10 @@
 // pair
 
 #include <iostream>
+#include <vector>
+#include <utility>  // for pair
+#include <string>  // for string
+
 using namespace std;
 
 int main() {
@@ -13,4 +17,17 @@ int main() {
     pair<int, char> p2 = {18, 'P'};
     cout << "First element: " << p2.first << endl;
     cout << "Second element: " << p2.second << endl;
+
+    // pair of pair
+    pair<int, pair<float, char>> p3 = {10, {15.5, 'F'}};
+    cout << "First element: " << p3.first << endl;
+    cout << "Second element: " << p3.second.first << endl;
+    cout << "Third element: " << p3.second.second << endl;
+
+
+    //vector pair
+    vector<pair<int, string>> vecPair = {{1, "John"}, {2, "David"}, {3, "Emma"}};
+    for (auto p : vecPair) {
+        cout << "First element: " << p.first << ", Second element: " << p.second << endl;
+    }
 }
