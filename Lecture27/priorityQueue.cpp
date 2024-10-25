7ff711810f83d3bf7a0e6a4decff07136fcab1fb
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-25 17:04:54 +0530

    // Function to print priority queue

--- Lecture27/priorityQueue.cpp
+++ Lecture27/priorityQueue.cpp
@@ -7,6 +7,17 @@
 using namespace std;
 
 
+// Function to print priority queue
+int void printQueue(priority_queue<int> queue) {
+    while(!queue.empty()) {
+        cout << queue.top() << " ";
+        queue.pop(); // remove the top element
+    }
+    cout << endl;
+ 
+}
+
+
 int main() {
     // Declare priority queue with integer data type
     priority_queue<int> pq;
