25ae6996125d0e4cdd10898ec73157bf6d80b9b8
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-25 17:03:49 +0530

    Declare priority queue with integer data type

---
+++ Lecture27/priorityQueue.cpp
@@ -0,0 +1,14 @@
+
+
+// Priority queue
+
+#include<iostream>
+#include<queue>
+using namespace std;
+
+
+int main() {
+    // Declare priority queue with integer data type
+    priority_queue<int> pq;
+
+}
