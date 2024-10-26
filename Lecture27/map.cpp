772f31d3afd06397fb44d53e2c7a9be659000aee
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-26 13:56:39 +0530

    Map value initilization

--- Lecture27/map.cpp
+++ Lecture27/map.cpp
@@ -7,7 +7,15 @@
 using namespace std;
 
 int main() {
-    map<string, int> studentMarks;
+    map<string, int> houseMaterials;
+    houseMaterials["TV"] = 5;
+    houseMaterials["Bed"] = 4;
+    houseMaterials["Window"] = 2;
+    houseMaterials["Door"] = 3;
+    houseMaterials["Roof"] = 1;
+    houseMaterials["Floor"] = 6;
+    houseMaterials["Fan"] = 10;
+    houseMaterials["Heater"] = 1;
 
 
 
