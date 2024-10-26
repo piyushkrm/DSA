2e9edfffc374a11e1d221c1393bbc1ffae88dcb2
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-26 15:57:44 +0530

    Sort an array

--- Lecture27/algorithms.cpp
+++ Lecture27/algorithms.cpp
@@ -6,6 +6,20 @@
 
 using namespace std;
 
+// print elements of array
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
 int main() {
-    
+    //Sorting
+    int arr[] = {5, 2, 8, 1, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    sort(arr, arr + n);
+    cout << "Sorted array in ascending order: ";
+    printArray(arr, n);
 }
