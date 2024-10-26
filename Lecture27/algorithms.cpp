d949c2490db940c0c722a2e0303d3bdf8ad90b2c
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-26 16:03:54 +0530

    Next permutation

--- Lecture27/algorithms.cpp
+++ Lecture27/algorithms.cpp
@@ -1,13 +1,12 @@
-
 // Algorithms
 
 #include <iostream>
 #include <algorithm>
+#include <iterator>
 
 using namespace std;
 
-// print elements of array
-
+// Print elements of the array
 void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";
@@ -16,15 +15,31 @@ void printArray(int arr[], int size) {
 }
 
 int main() {
-    //Sorting
+    // Sorting
     int arr[] = {5, 2, 8, 1, 3};
     int n = sizeof(arr) / sizeof(arr[0]);
+    
+    // Sort array in ascending order
     sort(arr, arr + n);
     cout << "Sorted array in ascending order: ";
     printArray(arr, n);
 
-    //  sort arrays with decreasing order
+    // Sort array in descending order
     sort(arr, arr + n, greater<int>());
     cout << "Sorted array in descending order: ";
     printArray(arr, n);
+
+    // Reverse array
+    reverse(arr, arr + n);
+    cout << "Reversed array: ";
+    printArray(arr, n);
+
+    // Next permutation
+    int arr2[] = {1, 2, 3};
+    int n2 = sizeof(arr2) / sizeof(arr2[0]);
+    next_permutation(arr2, arr2 + n2);
+    cout << "Next permutation: ";
+    printArray(arr2, n2);
+
+    return 0;
 }
