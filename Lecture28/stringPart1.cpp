4571ae4fdd88d205c04a0828e3a9c91709aed598
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-27 14:51:44 +0530

    Input and output string with basics cin function

--- Lecture28/stringPart1.cpp
+++ Lecture28/stringPart1.cpp
@@ -10,5 +10,13 @@ using namespace std;
 int main() {
     char str[100] = "Hello, World!";
     cout << "Original string: " << str << endl;
+
+    // Input and output the string
+    char str2[100];
+    cout << "Enter a string: ";
+    cin >> str2;
+    cout << "String entered: " << str2 << endl;
+
+
     return 0;
 }
