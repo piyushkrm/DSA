37fcf7c5a647edb97a4bb00d67f8086188db7d26
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-28 10:42:53 +0530

    String concatenation

--- Lecture28/stringPart1.cpp
+++ Lecture28/stringPart1.cpp
@@ -8,19 +8,19 @@
 using namespace std;
 
 int main() {
-    char str[100] = "Hello, World!";
-    cout << "Original string: " << str << endl;
+    // char str[100] = "Hello, World!";
+    // cout << "Original string: " << str << endl;
 
-    // Input and output the string
-    char str2[100];
-    cout << "Enter a string: ";
-    cin >> str2;
-    cout << "String entered: " << str2 << endl;
+    // // Input and output the string
+    // char str2[100];
+    // cout << "Enter a string: ";
+    // cin >> str2;
+    // cout << "String entered: " << str2 << endl;
 
-    // Input and output for character arrays
-    char str3[100];
-    cin.getline(str3, 100);
-    cout << "Character array entered: " << str3 << endl;
+    // // Input and output for character arrays
+    // char str3[100];
+    // cin.getline(str3, 100);
+    // cout << "Character array entered: " << str3 << endl;
 
     //String function or operations
     string st1 = "Piyush";
@@ -29,6 +29,10 @@ int main() {
     // length of strings
     cout << "Length of st1: " << st1.length() << endl;
     cout << "Length of st2: " << st2.length() << endl;
+
+    // String concatenation
+    string st3 = st1 + " " + st2;
+    cout << "Concatenated string: " << st3 << endl;
     
 
 
