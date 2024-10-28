072fd661a2a60ddbe4347a4bfa138b6b9bb445e1
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-28 11:11:38 +0530

    HomeWork : Find if a string is a string is palindrome or not.

--- Lecture28/stringPart1.cpp
+++ Lecture28/stringPart1.cpp
@@ -8,22 +8,43 @@
 
 using namespace std;
 
+// Approach 1 
+bool isPalindrome1(string str) {
+    int start = 0;
+    int end = str.length() - 1;
+    while(start < end) {
+        if(str[start++]!= str[end--]) {
+            return false;
+        } 
+    }
+    return true;
+}
+
+// Approach 2
+bool isPalindrome2(string str) {
+    string strRev = str;
+    reverse(strRev.begin(), strRev.end());
+    return str == strRev;
+}
+
 int main() {
-    // char str[100] = "Hello, World!";
-    // cout << "Original string: " << str << endl;
 
-    // // Input and output the string
-    // char str2[100];
-    // cout << "Enter a string: ";
-    // cin >> str2;
-    // cout << "String entered: " << str2 << endl;
 
-    // // Input and output for character arrays
-    // char str3[100];
-    // cin.getline(str3, 100);
-    // cout << "Character array entered: " << str3 << endl;
+    char str[100] = "Hello, World!";
+    cout << "Original string: " << str << endl;
 
-    //String function or operations
+    // Input and output the string
+    char str2[100];
+    cout << "Enter a string: ";
+    cin >> str2;
+    cout << "String entered: " << str2 << endl;
+
+    // Input and output for character arrays
+    char str3[100];
+    cin.getline(str3, 100);
+    cout << "Character array entered: " << str3 << endl;
+
+    String function or operations
     string st1 = "Piyush";
     string st2 = "Mishra";
 
@@ -70,6 +91,18 @@ int main() {
     string s2 = "CodeWithPihu";
     reverse(s2.begin(), s2.end());
     cout << "Reversed string: " << s2 << endl;
+
+
+    // HomeWork : Find if a string is a string is palindrome or not.
+    // Approach 1
+    string str01 = "level";
+    string str02 = "car";
+    cout << "Is palindrome: " << (isPalindrome1(str01)? "Yes" : "No") << endl;
+    cout << "Is palindrome: " << (isPalindrome1(str02)? "Yes" : "No") << endl;
+
+    // Approach 2 : Reverse the string and compare with original
+    cout << "Is palindrome: " << (isPalindrome2(str01)? "Yes" : "No") << endl;
+    cout << "Is palindrome: " << (isPalindrome2(str02)? "Yes" : "No") << endl;
 
     return 0;
 }
