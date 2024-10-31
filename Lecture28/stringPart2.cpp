b491ad17ee643528f880e00bce9af0eaed4be8ce
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-31 09:49:39 +0530

    Leetcode problem no 125 is solved successfully

--- Lecture28/stringPart2.cpp
+++ Lecture28/stringPart2.cpp
@@ -3,4 +3,42 @@
 // String part 2
 // Valid palindrome     Leet code 125
 
+#include <iostream>
+#include <algorithm>
+#include <string>
 
+using namespace std;
+
+bool isAlphaNumeric(char ch) {
+    return (ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'z');
+}
+
+bool isPalindrome(string str) {
+    int start = 0;
+    int end = str.length() - 1;
+
+    while (start < end) {
+        if (!isAlphaNumeric(tolower(str[start]))) {
+            start++;
+            continue;
+        }
+
+        if (!isAlphaNumeric(tolower(str[end]))) {
+            end--;
+            continue;
+        }
+
+        if (tolower(str[start])  != tolower(str[end])) {
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
+}
+
+int main() {
+    //  Call the isPalindrome() function
+    string str = "Ac3?e3c&a";
+    cout << "Is the string palindrome? " << (isPalindrome(str) ? "Yes" : "No") << endl;
+}
