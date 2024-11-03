522f3e8e57f3ab5979bb7915d8423e384bad7c13
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-03 12:53:54 +0530

    reverse word function complete and run successfully

--- Lecture31/removeWordInString.cpp
+++ Lecture31/removeWordInString.cpp
@@ -9,13 +9,22 @@ using namespace std;
 
 string removeWord(string str) {
     int n = str.length();
-    // string ans = "";
+    string ans = "";
     reverse(str.begin(), str.end());
-    return str;
-
+    for (int i = 0; i < n; i++) {
+        string word = "";
+        while (i < n && str[i] != ' ') {
+            word += str[i];
+            i++;
+        }
+        reverse(word.begin(), word.end());
+        if (word.length() > 0) {
+            ans += " "+ word;
+        }
+    }
+    return ans.substr(1);
 }
 
 int main() {
-    string s = "The sky is blue";
-    cout << removeWord(s) << endl;
+
 }
