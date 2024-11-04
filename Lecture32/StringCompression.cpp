823ddab0fb966f569f3711a8cdbdee3300b173f2
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-04 11:35:34 +0530

    compressionString function are complete

--- Lecture32/StringCompression.cpp
+++ Lecture32/StringCompression.cpp
@@ -6,6 +6,31 @@
 
 using namespace std;
 
-int main() {
+int compressString(string &s){
+    int idx = 0;
+    for (int i = 0; i < s.length(); i++) {
+        char ch = char[i]
+        int count = 1;
+        while (i < s.length() && char[i] == ch) {
+            i++;
+            count++;
+        }
+        if (count == 1) {
+            s[idx++] = ch;
+        } else {
+            s[idx++] = ch;
+            string str = to_string(count);
+            for (char digit : str) {
+                s[idx++] = digit;
+            }
+            i--; // Backtrack to the last character of the current group
+        }
+    }
+    s.resize(idx);
+    return idx;
+}
+
+int main()
+{
     string s = "aaabbcccccaaa";
 }
