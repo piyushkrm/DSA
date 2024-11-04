56f0133d3f04bd7e7f9afdf96c9c7f75d20134c5
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-04 16:38:02 +0530

    compressionString function are done and ready to run

---
+++ Lecture32/StringCompressionIII.cpp
@@ -0,0 +1,34 @@
+
+
+// 3163. String Compression III
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string compressString(string word) {
+    string comparison;      // store comparison string 
+    int n = word.length();
+
+    if (n == 0) return "";  //Return empty string
+
+    for(int i = 0; i < n;) {
+        char ch = word[i];
+        int count = 0;
+
+        // consecutive character count up maximum 9
+        while(i < n && word[i] == ch && count < 9) {
+            count ++;
+            i++;
+        }
+
+        // append character and count to comparison string
+        comparison += to_string(count) + ch;
+    }
+
+    return comparison;  // return comparison string
+}
+int main() {
+
+}
