4c09a32d9bdb8e7468b8a161f6f95cc161cc1662
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-04 11:43:39 +0530

    Some updation needed and now code all correct and run successfully

--- Lecture32/StringCompression.cpp
+++ Lecture32/StringCompression.cpp
@@ -3,34 +3,42 @@
 // 443. String Compression
 
 #include <iostream>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
 int compressString(string &s){
     int idx = 0;
-    for (int i = 0; i < s.length(); i++) {
-        char ch = char[i]
-        int count = 1;
-        while (i < s.length() && char[i] == ch) {
+    for (int i = 0; i < s.length();) {
+        char ch = s[i];
+        int count = 0;
+
+        // Count consecutive characters
+        while (i < s.length() && s[i] == ch) {
             i++;
             count++;
         }
-        if (count == 1) {
-            s[idx++] = ch;
-        } else {
-            s[idx++] = ch;
+
+        // Store the character
+        s[idx++] = ch;
+
+        // Store the count if it's more than 1
+        if (count > 1) {
             string str = to_string(count);
             for (char digit : str) {
                 s[idx++] = digit;
             }
-            i--; // Backtrack to the last character of the current group
         }
     }
     s.resize(idx);
     return idx;
 }
 
-int main()
-{
+int main() {
     string s = "aaabbcccccaaa";
+    int compressedSize = compressString(s);
+    cout << "Compressed string: " << s << endl;
+    cout << "Compressed size: " << compressedSize << endl;
+    return 0;
 }
