b2bc2d0854aaa4a9f4f3586f11b49250317d4282
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-06 16:01:00 +0530

    add documentation

--- Lecture33/primeNumber.cpp
+++ Lecture33/primeNumber.cpp
@@ -1,3 +1,17 @@
+/**
+ * The given C++ program uses the Sieve of Eratosthenes algorithm to efficiently find and count prime
+ * numbers within a specified range.
+ * 
+ * @param n The code you provided implements the Sieve of Eratosthenes algorithm to count the number of
+ * prime numbers in a given range from 2 to a specified upper limit `n`. The `countPrimes` function
+ * initializes a vector `isPrime` to track whether each number is prime or not
+ * 
+ * @return The `countPrimes` function returns the number of prime numbers in the range from 2 to the
+ * specified upper limit `n`.
+ */
+
+// Best approach to finding the prime number in a range  with Sieve of Eratosthenes algorithm
+
 #include <iostream>
 #include <vector>
 
