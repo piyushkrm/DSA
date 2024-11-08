0651f26f4509f2a552fa3afff944e173deca6612
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-08 12:51:33 +0530

    Diagonal sum of matrix

--- Lecture34/maxRowColSum.cpp
+++ Lecture34/maxRowColSum.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 // function to return the maximum row of sum
-int get_max_row_of_sum(int matrix[][3], int row, int column) {
+int get_max_row_of_sum(int matrix[][4], int row, int column) {
     int maxSum = INT_MIN;
 
     for (int i = 0; i < row; i++) {
@@ -23,7 +23,7 @@ int get_max_row_of_sum(int matrix[][3], int row, int column) {
 }
 
 // function to return the maximum column of sum
-int get_max_column_of_sum(int matrix[][3], int row, int column) {
+int get_max_column_of_sum(int matrix[][4], int row, int column) {
     int maxSum = INT_MIN;
 
     for (int j = 0; j < column; j++) {
@@ -36,17 +36,37 @@ int get_max_column_of_sum(int matrix[][3], int row, int column) {
     return maxSum;
 }
 
+// Diagonal sum of matrix
+int diagonalSum(int matrix[][4], int n) {
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        // Add main diagonal element
+        sum += matrix[i][i];
+
+        // Add anti-diagonal element, avoiding double-counting the center
+        if (i != n - i - 1) {
+            sum += matrix[i][n - i - 1];
+        }
+    }
+    
+    return sum;
+}
+
 int main() {
-        int matrix[4][3] = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9},
-        {10, 11, 12}
+        int matrix[4][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}
     };
 
     int maxRowSum = get_max_row_of_sum(matrix, 4, 3);
     cout << "Maximum row sum: " << maxRowSum << endl;
     int maxColumnSum = get_max_column_of_sum(matrix, 4, 3);
     cout << "Maximum column sum: " << maxColumnSum << endl;
+
+    cout << "Diagonal sum: " << diagonalSum(matrix, 4) << endl;
+
     return 0;
 }
