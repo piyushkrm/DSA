d69a94e809f96da3da22bb158553b5f512275a0a
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-08 16:18:10 +0530

    searhRow function, that is used for search on the column

--- Lecture35/search2DMatrix.cpp
+++ Lecture35/search2DMatrix.cpp
@@ -11,6 +11,32 @@
 #include <iostream>
 #include <vector>
 
+// Search on row
+bool searchRow(vector<vector<int>> matrix, int target, int row)
+{
+    int n = matrix[0].size();
+    int start = 0;
+    int end = n - 1;
+
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        if (target == matrix[row][mid])
+        { // found the target
+            return true;
+        }
+        else if (target > matrix[row][mid])
+        { // right
+            start = mid + 1;
+        }
+        else
+        { // left
+            end = mid - 1;
+        }
+    }
+    return false;
+}
+
 bool searchMatrix(vector<vector<int>> &matrix, int target)
 {
     // Binary Search
