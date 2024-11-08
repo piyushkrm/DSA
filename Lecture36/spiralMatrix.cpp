9957585ec2d4bad9cfbea81c7155cdd266031b68
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-08 16:48:24 +0530

    spiralOrder function are almost ready to run

--- Lecture36/spiralMatrix.cpp
+++ Lecture36/spiralMatrix.cpp
@@ -7,12 +7,54 @@
 
 using namespace std;
 
+// Function to print all the elements in spiral order of the matrix
+vector<int> spiralOrder(vector<vector<int>> & matrix) {
+    int m = matrix.size();
+    int n = matrix[0].size();
+    int startRow = 0;
+    int startCol = 0;
+    int endRow = m - 1;
+    int endCol = m - 1;
+    
+    vector<int> ans;
+
+    while(startRow <= endRow && startCol <= endCol) {
+        // For top boundary
+        for (int j = startCol; j < endCol; j++) {
+            ans.push_back(matrix[startRow][j]);
+        }
+
+        // Right boundary
+        for (int i = startRow; i < endRow; i++) {
+            ans.push_back(matrix[i][endCol]);
+        }
+
+        // Bottom boundary
+        for (int j = endCol - 1; j >= startCol; j--) {
+            if (startRow == endRow) {
+                break;
+            }
+            ans.push_back(matrix[endRow][j]);
+        }
+
+        // Left boundary
+        for (int i = endRow - 1; i >= startRow + 1; i--) {
+            if (startCol == endCol) {
+                break;
+            }
+            ans.push_back(matrix[i][startRow]);
+        }
+    }
+    return ans;
+}
+
 int main() {
-    int matrix[][] = {
+    int matrix[][4] = {
         {1, 2, 3, 4},
         {5, 6, 7, 8},
         {9, 10, 11, 12},
         {13, 14, 15, 16}
-    }
+    };
+    cout << spiralOrder(matrix) << endl;
     
 }
