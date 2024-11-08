ecbe4e19fc5b80028306daeda9cb5f28c7ddd509
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-08 16:35:09 +0530

    main function almost done

---
+++ Lecture36/spiralMatrix.cpp
@@ -0,0 +1,18 @@
+
+
+// Spiral Matrix Leet code problem 54
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int matrix[][] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}
+    }
+    
+}
