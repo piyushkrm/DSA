7c0ece29659b28964cac28d72e9a61fe05ee908b
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-08 11:28:48 +0530

    Linear search on 2D arrays

---
+++ Lecture34/linearSearch.cpp
@@ -0,0 +1,32 @@
+
+
+// Linear Seach
+
+#include <iostream>
+
+using namespace std;
+
+bool linear_seach(int matrix[][3], int rows, int columns, int target) {
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            if (matrix[i][j] == target) {
+                return true;
+            }
+        }
+    }
+    return false;  // Target not found in the matrix  // Time complexity: O(n^2)
+}
+
+int main() {
+    int matrix[4][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+        {10, 11, 12}
+    };
+
+    int target = 9;
+    cout << (linear_seach(matrix, 4, 3, target)? "Target found" : "Target not found") << endl;
+    return 0;
+}
