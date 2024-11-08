f0461ddf134491f94d23ddda60e3d931f58dc67d
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-08 11:56:54 +0530

    return the pair of the target

--- Lecture34/linearSearch.cpp
+++ Lecture34/linearSearch.cpp
@@ -6,6 +6,20 @@
 
 using namespace std;
 
+// Linear Seach and return as a pair of index
+
+pair<int, int> linear_seach_pair(int matrix[][3], int rows, int columns, int target) {
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            if (matrix[i][j] == target) {
+                return make_pair(i, j);
+            }
+        }
+    }
+    return make_pair(-1, -1);  // Target not found in the matrix  // Time complexity: O(n^2)
+}
+
 bool linear_seach(int matrix[][3], int rows, int columns, int target) {
 
     for (int i = 0; i < rows; i++) {
@@ -28,5 +42,8 @@ int main() {
 
     int target = 9;
     cout << (linear_seach(matrix, 4, 3, target)? "Target found" : "Target not found") << endl;
+    pair<int, int> p = linear_seach_pair(matrix, 4, 3, target);
+    cout << "Pair of the target is : (" << p.first << ", " << p.second <<")" << endl;
+    
     return 0;
 }
