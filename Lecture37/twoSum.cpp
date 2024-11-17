930286976393afd5145b36b4c460ee775bcd91b8
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-17 12:04:32 +0530

    two sum problem are done

--- Lecture37/twoSum.cpp
+++ Lecture37/twoSum.cpp
@@ -14,8 +14,23 @@
 
 using namespace std;
 
+vector<int> twoSum(vector<int> vector, int target) {
+    int n = vector.size();
+    for (int i = 0; i < n; i++) {
+        int first = vector[i];
+        for (int j = i + 1; j < n; j++) {
+            int second = vector[j];
+            if (first + second == target) {
+                return {i, j};
+            }
+        }
+    }
+    return {};  // No pair found
+}
 int main() {
     vector<int> nums = {2, 7, 11, 15};
     int target = 9;
-    
+    vector<int> result = twoSum(nums, target);
+    cout << "Indices of the two numbers that add up to target: " << result[0] << ", " << result[1] << endl;
+    return 0;
 }
