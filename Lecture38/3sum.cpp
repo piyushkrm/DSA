06c01506086c0919a7cd999baca0ed7ad197a8f8
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-22 16:42:51 +0530

    Leetcode problem15, 3 sum problem

---
+++ Lecture38/3sum.cpp
@@ -0,0 +1,14 @@
+// leet code problem 15 3Sum
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+// Notice that the solution set must not contain duplicate triplets.
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int mai() {
+    
+}
