13735e818c8feb2cb44bbd787317b2adfed1bb53
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-22 17:08:29 +0530

    Approach 1 Brute force approach

--- Lecture38/3sum.cpp
+++ Lecture38/3sum.cpp
@@ -5,7 +5,8 @@
 
 #include <vector>
 #include <iostream>
-#include <algorithm>
+#include <set>  // For set data structure
+#include <algorithm>  // For sorting the triplet
 
 using namespace std;
 
@@ -13,19 +14,27 @@ using namespace std;
 vector<vector<int>> threeSum(vector<int>& nums) {
     int n = nums.size();
     vector<vector<int>> result;
+    set<vector<int>> st;
 
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
             for (int k = j + 1; k < n; k++) {
+                // Check if the sum of the three numbers is equal to zero
                 if (nums[i] + nums[j] + nums[k] == 0) {
-                    result.push_back({nums[i], nums[j], nums[k]});
+                    vector<int> trip = {nums[i], nums[j], nums[k]};
+                    sort(trip.begin(), trip.end());
+                    // Sort the triplet to remove duplicates
+                    if (st.find(trip) == st.end()) {
+                        st.insert(trip);  // Insert the triplet into the set
+                        result.push_back(trip);  // Add the triplet to the result vector
+                    }
                 }
             }
         }
     }
     return result;
 }
-int mai() {
+int main() {
     vector<int> nums = {-1, 0, 1, 2, -1, -4};
     vector<vector<int>> triplets = threeSum(nums);
     for (auto triplet : triplets) {
