85da86c3f585633c4f6b440337d75c88f9987731
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-22 16:45:40 +0530

    threeSum function almost complete

--- Lecture38/3sum.cpp
+++ Lecture38/3sum.cpp
@@ -9,6 +9,30 @@
 
 using namespace std;
 
+// Approach 1 Brute force approach 
+vector<vector<int>> threeSum(vector<int>& nums) {
+    int n = nums.size();
+    vector<vector<int>> result;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            for (int k = j + 1; k < n; k++) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    result.push_back({nums[i], nums[j], nums[k]});
+                }
+            }
+        }
+    }
+    return result;
+}
 int mai() {
-    
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+    vector<vector<int>> triplets = threeSum(nums);
+    for (auto triplet : triplets) {
+        for (int num : triplet) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+    return 0;
 }
