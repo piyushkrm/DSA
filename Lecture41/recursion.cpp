31e3139f5afc706b19b1736a7ced189449c564c1
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-26 11:31:35 +0530

    Recursion function to calculate the factorial of the given number

--- Lecture41/recursion.cpp
+++ Lecture41/recursion.cpp
@@ -13,11 +13,22 @@ int printNums(int n) {
     return printNums(n - 1);
 }
 
+// Recursion function to calculate the factorial of the given number
+int factPrint(int n) {
+    if (n == 0) return 1;
+    return n * factPrint(n - 1);
+}
+
 int main() {
     int n;
     cout << "Enter the number to print numbers from n to 1: ";
     cin >> n;
     printNums(n);
+
+    cout << "\nEnter the number to calculate its factorial: ";
+    cin >> n;
+    cout << "Factorial of " << n << " is: " << factPrint(n);
     
+
     return 0;
 }
