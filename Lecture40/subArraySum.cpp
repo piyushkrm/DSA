0079c5a725081e5c63c69d59e2affa38cc91cf27
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-26 11:23:22 +0530

    Optimize approach with prefix

--- Lecture40/subArraySum.cpp
+++ Lecture40/subArraySum.cpp
@@ -7,6 +7,8 @@
 #include <iostream>
 #include <vector>
 
+#include <unordered_map>
+
 using namespace std;
 
 // Brute force multiplication
@@ -24,9 +26,39 @@ int subarraySum(vector<int>& nums, int k) {
 
 }
 
+// Optimize approach with prefix
+int subarraySumPrefix(vector<int>& nums, int k) {
+    int count = 0;
+    int n = nums.size();
+    vector<int> prefixSum(n, 0);
+    prefixSum[0] = nums[0];
+    for (int i = 1; i < nums.size(); i++) {
+        prefixSum[i] = prefixSum[i - 1] + nums[i];
+    }
+    unordered_map<int, int> m;
+    for (int j = 0; j < nums.size(); j++) {
+        if (prefixSum[j] == k) count++;
+        int val = prefixSum[j] - k;
+
+        if (m.find(val) != m.end()) {
+            count += m[val];
+        }
+
+        if (m.find(prefixSum[j]) == m.end()) {
+            m[prefixSum[j]] = 0;
+        }
+        m[prefixSum[j]]++;
+    }
+    return count;
+    }
+
 int main() {
     vector<int> nums = {9, 4, 20, 3, 10, 5};
     int k = 33;
     cout << "Total number of subarrays with sum equal to " << k << ": " << subarraySum(nums, k) << endl;
+
+    // calling optimized function
+    cout << "Total number of subarrays with sum equal to " << k << ": " << subarraySumPrefix(nums, k) << endl;
+
     return 0;
 }
