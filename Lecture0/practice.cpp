5040af14705d79ac5f53bb1d5ce713fa1d195c9b
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-27 16:09:42 +0530

    1. Sum of two numbers

---
+++ Lecture0/practice.cpp
@@ -0,0 +1,22 @@
+
+
+// In this folder we are practice the all basics problem
+
+
+#include <iostream>
+
+using namespace std;
+
+// 1. Sum of two numbers
+
+int sumOfTwoNumbers(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    // 1 Sum of two numbers
+    int sum = sumOfTwoNumbers(5, 7);
+    cout << "Sum: " << sum << endl;
+
+    return 0;
+}
