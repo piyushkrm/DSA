a9ce38c7846dbb4229b7ae1c2a3578f0cbd96756
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-29 11:32:23 +0530

    getSubSet function complete

--- Lecture43/subsetsII.cpp
+++ Lecture43/subsetsII.cpp
@@ -9,6 +9,27 @@
 
 using namespace std;
 
+    void getSubSet(vector<int>& nums, vector<int>& ans, int i, vector<vector<int>>& allSubSet) {
+        if (nums.size() == i) {
+            allSubSet.push_back(ans);
+            return;
+        }
+
+        // Inclusion
+        ans.push_back(nums[i]);
+        getSubSet(nums, ans, i + 1, allSubSet);
+
+        // Backtracking
+        ans.pop_back();
+
+        // Skipping duplicates
+        int idx = i + 1;
+        while (idx < nums.size() && nums[idx] == nums[idx - 1]) idx++;
+
+        // Exclusion
+        getSubSet(nums, ans, idx, allSubSet);
+    }
+
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         vector<vector<int>> allSubSet;
