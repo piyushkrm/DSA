55c390e9859805537a23809c05555c41613730c5
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-12-03 19:36:01 +0530

    leet code problem 46. Permutations

---
+++ Lecture44/permulation.cpp
@@ -0,0 +1,44 @@
+// leet code problem 46. Permutations
+
+
+// Given an array nums of distinct integers, return all the possible permutations. 
+// You can return the answer in any order.
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void getPermutation(vector<int>& nums, int idx, vector<vector<int>>& result) {
+    if (idx == nums.size()) {
+        result.push_back(nums);
+    }
+
+    for (int i = idx; i < nums.size(); i++) {
+        swap(nums[idx], nums[i]);
+
+        getPermutation(nums, idx+1, result);
+        
+        swap(nums[idx], nums[i]);
+    }
+}
+
+vector<vector<int>> permute(vector<int>& nums) {
+    vector<vector<int>> result;
+    getPermutation(nums, 0, result);
+    return result;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3};
+    vector<vector<int>> result = permute(nums);
+
+    cout << "Permutations:" << endl;
+    for (const auto& permutation : result) {
+        for (int num : permutation) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
