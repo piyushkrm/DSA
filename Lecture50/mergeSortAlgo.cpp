2bee17e82ee10d409c4dcaa37f54fd681e801d54
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-12-20 18:39:42 +0530

    mergeSort function completed

--- Lecture50/mergeSortAlgo.cpp
+++ Lecture50/mergeSortAlgo.cpp
@@ -7,6 +7,16 @@
 
 using namespace std;
 
+// Function to divide array elements
+void mergeSort(arr[], st, end) {
+    if (st < end) {
+        int mid = st + (end - st) / 2;
+        mergeSort(arr, st, mid);
+        mergeSort(arr, mid + 1, end);
+        merge(arr, st, mid, end);
+    }
+}
+
 int main() {
     vector<int> arr = {12, 11, 13, 5, 6, 7};
     mergeSort(arr, 0, arr.size() - 1);
@@ -15,7 +25,7 @@ int main() {
         cout << val << endl;
     }
     cout << endl;
-    
+
     return 0;
 
 }
