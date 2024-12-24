c480c0c44efc3b0ef4f50014c73f69e7d2a6c96e
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-12-24 13:10:14 +0530

    In Optimal implementation merge sort function complete

--- Lecture52/countInversionProblem.cpp
+++ Lecture52/countInversionProblem.cpp
@@ -22,6 +22,20 @@ int countInversions(vector<int>& nums) {
     return count;
 }
 
+// Optimal implementation
+
+int mergeSort(vector<int>& arr, int st, int end) {
+    if (st <= end) {
+        int mid = (st + end) / 2;
+        int left = mergeSort(arr, st, mid);
+        int right = mergeSort(arr, mid + 1, end);
+        int current = merge(arr, st, mid, end);
+
+        return left + right + current;
+    }
+    return 0;
+}
+
 int main() {
     vector<int> nums = {8, 4, 2, 1, 5, 6, 3};
     vector<int> counts = {6, 3, 5, 2, 7};
