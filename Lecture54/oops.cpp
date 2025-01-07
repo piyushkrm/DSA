db65ecb2f698bd5e7166c4df902efff6060663b4
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2025-01-07 17:06:10 +0530

    Getter and setter to retrieve the private properties of class

--- Lecture54/oops.cpp
+++ Lecture54/oops.cpp
@@ -2,6 +2,7 @@
  * This program demonstrates the use of classes and objects in C++.
  * It defines a `Teacher` class with attributes like `name`, `department`, `salary`, and `id`,
  * and includes a method to display the information of a teacher.
+ * It also demonstrates encapsulation by using private data members with getter and setter methods.
  */
 
 #include <iostream>
@@ -9,18 +10,32 @@ using namespace std;
 
 // Teacher class declaration
 class Teacher {
-public: // Access Modifier
+private:
+    string passKey; // Private password attribute
+
+public:
     string name;        // Name of the teacher
     string department;  // Department of the teacher
     double salary;      // Salary of the teacher
     int id;             // ID of the teacher
 
+    // Setter for passKey
+    void setPasskey(string key) {
+        passKey = key;
+    }
+
+    // Getter for passKey
+    string getPasskey() {
+        return passKey;
+    }
+
     // Method to display teacher information
     void getInformation() {
         cout << "Name: " << name << endl;
         cout << "Department: " << department << endl;
-        cout << "Salary: " << salary << endl;  // Fixed missing colon
+        cout << "Salary: " << salary << endl;
         cout << "ID: " << id << endl;
+        cout << "Passkey (using getter): " << getPasskey() << endl; // Use getter for passKey
     }
 };
 
@@ -33,8 +48,11 @@ int main() {
     // Assign values to teacher object
     teacher.name = "Piyushu";
     teacher.department = "Teaching";
-    teacher.salary = 250000;  // Fixed spacing for readability
+    teacher.salary = 250000;
     teacher.id = 123;
+
+    // Set the private passkey
+    teacher.setPasskey("piyushuu");
 
     // Display the teacher's information
     teacher.getInformation();
