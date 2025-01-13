9066437af6da287197e46ad901b9717c7ca7e112
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2025-01-13 20:22:39 +0530

    LinkedList -->  Insertion at the beginning of the linked list (push_front in LinkedList)

---
+++ Lecture55/linkedList.cpp
@@ -0,0 +1,71 @@
+
+
+// Linked List
+
+#include <iostream>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data;   // data
+    Node *next; // pointer to next node
+
+    Node(int d)
+    { // constructor
+        data = d;
+        next = NULL;
+    }
+};
+
+class List
+{
+    Node *head;
+    Node *tail;
+
+public:
+    List()
+    {
+        head = NULL;
+        tail = NULL;
+    }
+    // Insertion at the beginning of the linked list (push_front in LinkedList)
+    void push_front(int val)
+    {
+        Node *newNode = new Node(val); // create a new node with given value
+        if (head == NULL)
+        { // if list is empty
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    // Print the list of nodes
+    void printList()
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+int main()
+{
+    List ll;
+    ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
+
+    cout << "Linked List: ";
+    ll.printList();
+}
